57edcc47df52b6298ec83b01a60e6cd7be112989
axexlck <axelclk@gmail.com>
2013-06-15 13:03:32 +0200

    Added special cases for quadratic and cubic polynomials to NRoots

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/convert/Expr2Object.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/convert/Expr2Object.java
@@ -15,6 +15,8 @@ import org.matheclipse.core.interfaces.ISymbol;
 import com.google.common.math.DoubleMath;
 
 public class Expr2Object {
+	public static int MAX_POLYNOMIAL = 4;
+
 	public static double[] toDoubleVector(IAST ast) throws WrongArgumentType {
 		double[] result = new double[ast.size() - 1];
 		for (int i = 1; i < ast.size(); i++) {
@@ -43,6 +45,24 @@ public class Expr2Object {
 		return result;
 	}
 
+	public static double[] toPolynomial(IExpr expr, ISymbol sym) {
+		double[] array = new double[MAX_POLYNOMIAL];
+		Map<Integer, Double> map = toPolynomialMap(expr, sym);
+		if (map == null) {
+			return null;
+		}
+		int k;
+		for (Map.Entry<Integer, Double> entry : map.entrySet()) {
+			k = entry.getKey();
+			if (k <= 4) {
+				array[k] = entry.getValue();
+			} else {
+				return null;
+			}
+		}
+		return array;
+	}
+
 	/**
 	 * 
 	 * @param expr
@@ -50,7 +70,7 @@ public class Expr2Object {
 	 * @return <code>null</code> if the expression couldn't be converted to a
 	 *         polynomial.
 	 */
-	public static Map<Integer, Double> toPolynomialFunction(IExpr expr, ISymbol sym) {
+	public static Map<Integer, Double> toPolynomialMap(IExpr expr, ISymbol sym) {
 		try {
 			Map<Integer, Double> map = new HashMap<Integer, Double>();
 			if (expr.isPlus()) {
@@ -58,9 +78,49 @@ public class Expr2Object {
 				for (int i = 1; i < plus.size(); i++) {
 					if (plus.get(i).isTimes()) {
 						IAST times = (IAST) plus.get(i);
+						double coeff = 0;
+						int exp = -1;
 						for (int j = 1; j < times.size(); j++) {
-
+							if (times.get(j).isPower()) {
+								IAST power = (IAST) times.get(j);
+								if (power.get(1).equals(sym)) {
+									if (exp != (-1)) {
+										return null;
+									}
+									IExpr res = F.evaln(power.get(2));
+									if (!(res instanceof INum)) {
+										return null;
+									}
+									exp = DoubleMath.roundToInt(((INum) res).doubleValue(), RoundingMode.UNNECESSARY);
+									if (exp < 0) {
+										return null;
+									}
+									continue;
+								}
+							} else if (times.get(j).isSymbol()) {
+								if (times.get(j).equals(sym)) {
+									if (exp != (-1)) {
+										return null;
+									}
+									exp = 1;
+									continue;
+								}
+							}
+							if (times.get(j) instanceof INum) {
+								coeff += ((INum) times.get(j)).doubleValue();
+								continue;
+							}
+							IExpr res = F.evaln(times.get(j));
+							if (!(res instanceof INum)) {
+								return null;
+							}
+							coeff += ((INum) res).doubleValue();
 						}
+						if (exp == (-1)) {
+							exp = 0;
+						}
+						addCoefficient(map, coeff, exp);
+						continue;
 					} else if (plus.get(i).isPower()) {
 						IAST power = (IAST) plus.get(i);
 						if (power.get(1).equals(sym)) {
@@ -69,22 +129,29 @@ public class Expr2Object {
 								return null;
 							}
 							int exp = DoubleMath.roundToInt(((INum) res).doubleValue(), RoundingMode.UNNECESSARY);
-							map.put(Integer.valueOf(exp), Double.valueOf(1.0));
+							if (exp < 0) {
+								return null;
+							}
+							addCoefficient(map, 1.0, exp);
 							continue;
 						}
 						return null;
 					} else if (plus.get(i).isSymbol()) {
 						if (plus.get(i).equals(sym)) {
-							map.put(Integer.valueOf(1), Double.valueOf(1.0));
+							addCoefficient(map, 1.0, 1);
 							continue;
 						}
 						return null;
+					}
+					if (plus.get(i) instanceof INum) {
+						addCoefficient(map, ((INum) plus.get(i)).doubleValue(), 0);
+						continue;
 					}
 					IExpr res = F.evaln(plus.get(i));
 					if (!(res instanceof INum)) {
 						return null;
 					}
-					map.put(Integer.valueOf(0), Double.valueOf(((INum) res).doubleValue()));
+					addCoefficient(map, ((INum) res).doubleValue(), 0);
 				}
 				return map;
 			}
@@ -92,5 +159,15 @@ public class Expr2Object {
 
 		}
 		return null;
+	}
+
+	private static void addCoefficient(Map<Integer, Double> map, double v, int k) {
+		Integer key = Integer.valueOf(k);
+		Double value = map.get(key);
+		if (value == null) {
+			map.put(Integer.valueOf(k), Double.valueOf(v));
+		} else {
+			map.put(Integer.valueOf(k), Double.valueOf(value.doubleValue() + v));
+		}
 	}
 }
--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/NRoots.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/NRoots.java
@@ -1,36 +1,15 @@
 package org.matheclipse.core.reflection.system;
 
-import static org.matheclipse.core.expression.F.C0;
-import static org.matheclipse.core.expression.F.C1;
-import static org.matheclipse.core.expression.F.C2;
-import static org.matheclipse.core.expression.F.CI;
 import static org.matheclipse.core.expression.F.List;
-import static org.matheclipse.core.expression.F.Plus;
-import static org.matheclipse.core.expression.F.Power;
-import static org.matheclipse.core.expression.F.Sqr;
-import static org.matheclipse.core.expression.F.Sqrt;
-import static org.matheclipse.core.expression.F.Subtract;
-import static org.matheclipse.core.expression.F.Times;
 import static org.matheclipse.core.expression.F.evalExpandAll;
-import static org.matheclipse.core.expression.F.fraction;
-import static org.matheclipse.core.expression.F.integer;
 
-import java.util.SortedMap;
-
+import org.matheclipse.core.convert.Expr2Object;
 import org.matheclipse.core.convert.ExprVariables;
-import org.matheclipse.core.convert.JASConvert;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
-import org.matheclipse.core.interfaces.IFraction;
-import org.matheclipse.core.interfaces.IInteger;
-
-import edu.jas.arith.BigRational;
-import edu.jas.poly.GenPolynomial;
-import edu.jas.poly.Monomial;
-import edu.jas.ufd.FactorAbstract;
-import edu.jas.ufd.FactorFactory;
+import org.matheclipse.core.interfaces.ISymbol;
 
 /**
  * Determine the numerical roots of a univariate polynomial
@@ -73,6 +52,18 @@ public class NRoots extends AbstractFunctionEvaluator {
 		}
 		IExpr expr = evalExpandAll(ast.get(1));
 		IAST variables = eVar.getVarList();
+		ISymbol sym = (ISymbol) variables.get(1);
+		double[] poly = Expr2Object.toPolynomial(expr, sym);
+		IAST result = null;
+		if (poly != null) {
+			if (poly[3] == 0.0) {
+				return quadratic(poly[2], poly[1], poly[0]);
+			}
+			result = cubic(poly[3], poly[2], poly[1], poly[0]);
+			if (result != null) {
+				return result;
+			}
+		}
 		IExpr denom = F.C1;
 		if (expr.isAST()) {
 			expr = Together.together((IAST) expr);
@@ -84,20 +75,108 @@ public class NRoots extends AbstractFunctionEvaluator {
 				expr = F.eval(F.Numerator(expr));
 			}
 		}
-		return rootsOfVariable(expr, denom, variables);
+		return rootsOfVariable(expr, denom);
 	}
 
-	protected static IAST rootsOfVariable(final IExpr expr, final IExpr denom, final IAST variables) {
+	protected static IAST rootsOfVariable(final IExpr expr, final IExpr denom) {
 		IAST result = List();
 		IAST resultList = RootIntervals.croots(expr, true);
-		if (resultList == null) {
-			return null;
+		if (resultList != null) {
+			if (resultList.size() > 0) {
+				result.addAll(resultList);
+			}
+			return result;
 		}
-		if (resultList.size() > 0) {
-			result.addAll(resultList);
-		}
-		return result;
-
+		return null;
 	}
 
+	private static IAST quadratic(double a, double b, double c) {
+		IAST result = F.List();
+		double discriminant = (b * b - (4 * a * c));
+		if (discriminant == 0.0) {
+			double bothEqual = ((-b / (2.0 * a)));
+			result.add(F.num(bothEqual));
+			result.add(F.num(bothEqual));
+		} else if (discriminant < 0.0) {
+			// two complex roots
+			double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+			double realPart = (-b / (2.0 * a));
+			result.add(F.complex(realPart, imaginaryPart));
+			result.add(F.complex(realPart, -imaginaryPart));
+		} else {
+			// two real roots
+			double real1 = ((-b + Math.sqrt(discriminant)) / (2.0 * a));
+			double real2 = ((-b - Math.sqrt(discriminant)) / (2.0 * a));
+			result.add(F.num(real1));
+			result.add(F.num(real2));
+		}
+		return result;
+	}
+
+	/**
+	 * See <a href=
+	 * "http://stackoverflow.com/questions/13328676/c-solving-cubic-equations"
+	 * >http
+	 * ://stackoverflow.com/questions/13328676/c-solving-cubic-equations</a>
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @param d
+	 */
+	private static IAST cubic(double a, double b, double c, double d) {
+		if (a == 0) {
+			return null;
+		}
+		if (d == 0) {
+			return null;
+		}
+		IAST result = F.List();
+		b /= a;
+		c /= a;
+		d /= a;
+
+		double q = (3.0 * c - (b * b)) / 9.0;
+		double r = -(27.0 * d) + b * (9.0 * c - 2.0 * (b * b));
+		r /= 54.0;
+		double discriminant = q * q * q + r * r;
+
+		double term1 = (b / 3.0);
+		if (discriminant > 0) {
+			// one root real, two are complex
+			double s = r + Math.sqrt(discriminant);
+			s = ((s < 0) ? -Math.pow(-s, (1.0 / 3.0)) : Math.pow(s, (1.0 / 3.0)));
+			double t = r - Math.sqrt(discriminant);
+			t = ((t < 0) ? -Math.pow(-t, (1.0 / 3.0)) : Math.pow(t, (1.0 / 3.0)));
+			result.add(F.num(-term1 + s + t));
+			term1 += (s + t) / 2.0;
+			double realPart = -term1;
+			term1 = Math.sqrt(3.0) * (-t + s) / 2;
+			result.add(F.complex(realPart, term1));
+			result.add(F.complex(realPart, -term1));
+			return result;
+		}
+
+		// The remaining options are all real
+		double r13;
+		if (discriminant == 0) {
+			// All roots real, at least two are equal.
+			r13 = ((r < 0) ? -Math.pow(-r, (1.0 / 3.0)) : Math.pow(r, (1.0 / 3.0)));
+			result.add(F.num(-term1 + 2.0 * r13));
+			result.add(F.num(-(r13 + term1)));
+			result.add(F.num(-(r13 + term1)));
+			return result;
+		}
+
+		// Only option left is that all roots are real and unequal (to get here,
+		// q < 0)
+		q = -q;
+		double dum1 = q * q * q;
+		dum1 = Math.acos(r / Math.sqrt(dum1));
+		r13 = 2.0 * Math.sqrt(q);
+		result.add(F.num(-term1 + r13 * Math.cos(dum1 / 3.0)));
+		result.add(F.num(-term1 + r13 * Math.cos((dum1 + 2.0 * Math.PI) / 3.0)));
+		result.add(F.num(-term1 + r13 * Math.cos((dum1 + 4.0 * Math.PI) / 3.0)));
+		return result;
+	}
 }
