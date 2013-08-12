285b0288ce2dba9e77ea43b0ba7e409f2ea55537
axexlck <axelclk@gmail.com>
2013-08-12 23:06:35 +0200

    Inproved TrigToExp[] for hyperbolic functions

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/TrigToExp.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/TrigToExp.java
@@ -1,8 +1,31 @@
 package org.matheclipse.core.reflection.system;
 
+import static org.matheclipse.core.expression.F.$;
+import static org.matheclipse.core.expression.F.ArcCos;
+import static org.matheclipse.core.expression.F.ArcSin;
+import static org.matheclipse.core.expression.F.ArcTan;
+import static org.matheclipse.core.expression.F.C1;
+import static org.matheclipse.core.expression.F.C1D2;
+import static org.matheclipse.core.expression.F.CI;
+import static org.matheclipse.core.expression.F.CN1;
+import static org.matheclipse.core.expression.F.*;
+import static org.matheclipse.core.expression.F.Cos;
+import static org.matheclipse.core.expression.F.E;
+import static org.matheclipse.core.expression.F.Log;
+import static org.matheclipse.core.expression.F.Pi;
+import static org.matheclipse.core.expression.F.Plus;
+import static org.matheclipse.core.expression.F.Power;
+import static org.matheclipse.core.expression.F.Sin;
+import static org.matheclipse.core.expression.F.Sinh;
+import static org.matheclipse.core.expression.F.Sqr;
+import static org.matheclipse.core.expression.F.Sqrt;
+import static org.matheclipse.core.expression.F.Subtract;
+import static org.matheclipse.core.expression.F.Tan;
+import static org.matheclipse.core.expression.F.Tanh;
+import static org.matheclipse.core.expression.F.Times;
+
 import org.matheclipse.core.eval.exception.WrongNumberOfArguments;
 import org.matheclipse.core.eval.interfaces.IFunctionEvaluator;
-import static org.matheclipse.core.expression.F.*;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
@@ -11,8 +34,9 @@ import org.matheclipse.core.visit.VisitorExpr;
 /**
  * Exponential definitions for trigonometric functions
  * 
- * <a href="http://en.wikipedia.org/wiki/List_of_trigonometric_identities#Exponential_definitions"
- * >List of trigonometric identities - Exponential definitions</a>
+ * See <a href="http://en.wikipedia.org/wiki/List_of_trigonometric_identities#Exponential_definitions">List of trigonometric
+ * identities - Exponential definitions</a>,<br/>
+ * <a href="http://en.wikipedia.org/wiki/Hyperbolic_function">Hyperbolic function</a>
  */
 public class TrigToExp implements IFunctionEvaluator {
 
@@ -26,29 +50,54 @@ public class TrigToExp implements IFunctionEvaluator {
 
 		@Override
 		public IExpr visit2(IExpr head, IExpr arg1) {
-			IExpr temp = arg1;
+			IExpr x = arg1;
 			IExpr result = arg1.accept(this);
 			if (result != null) {
-				temp = result;
+				x = result;
 			}
 			if (head.equals(Sin)) {
-				return Subtract(Times(C1D2, CI, Power(E, Times(CNI, temp))), Times(C1D2, CI, Power(E, Times(CI, temp))));
+				return Subtract(Times(C1D2, CI, Power(E, Times(CNI, x))), Times(C1D2, CI, Power(E, Times(CI, x))));
 			}
 			if (head.equals(Cos)) {
-				return Plus(Times(C1D2, Power(E, Times(CNI, temp))), Times(C1D2, Power(E, Times(CI, temp))));
+				return Plus(Times(C1D2, Power(E, Times(CNI, x))), Times(C1D2, Power(E, Times(CI, x))));
 			}
 			if (head.equals(Tan)) {
-				return Times(CI, Subtract(Power(E, Times(CNI, temp)), Power(E, Times(CI, temp))), Power(Plus(Power(E, Times(CNI, temp)),
-						Power(E, Times(CI, temp))), CN1));
+				return Times(CI, Subtract(Power(E, Times(CNI, x)), Power(E, Times(CI, x))),
+						Power(Plus(Power(E, Times(CNI, x)), Power(E, Times(CI, x))), CN1));
 			}
 			if (head.equals(ArcSin)) {
-				return Times(CNI, Log(Plus(Sqrt(Subtract(C1, Sqr(temp))), Times(CI, temp))));
+				return Times(CNI, Log(Plus(Sqrt(Subtract(C1, Sqr(x))), Times(CI, x))));
 			}
 			if (head.equals(ArcCos)) {
-				return Plus(Times(C1D2, Pi), Times(CI, Log(Plus(Sqrt(Subtract(C1, Sqr(temp))), Times(CI, temp)))));
+				return Plus(Times(C1D2, Pi), Times(CI, Log(Plus(Sqrt(Subtract(C1, Sqr(x))), Times(CI, x)))));
 			}
 			if (head.equals(ArcTan)) {
-				return Subtract(Times(C1D2, CI, Log(Plus(C1, Times(CNI, temp)))), Times(C1D2, CI, Log(Plus(C1, Times(CI, temp)))));
+				return Subtract(Times(C1D2, CI, Log(Plus(C1, Times(CNI, x)))), Times(C1D2, CI, Log(Plus(C1, Times(CI, x)))));
+			}
+			if (head.equals(Cosh)) {
+				// JavaForm[(E^x+E^(-x))/2]
+				return Times(C1D2,Plus(Power(E,x),Power(E,Times(CN1,x))));
+			}
+			if (head.equals(Csch)) {
+				// JavaForm[2/(E^x-E^(-x))]
+				return Times(C2,Power(Plus(Power(E,x),Times(CN1,Power(E,Times(CN1,x)))),CN1));
+			}
+			if (head.equals(Coth)) {
+				// JavaForm[((E^(-x))+E^x)/((-E^(-x))+E^x)]
+				return Times(Plus(Power(E,x),Power(E,Times(CN1,x))),Power(Plus(Power(E,x),Times(CN1,Power(E,Times(CN1,x)))),CN1));
+			}
+			if (head.equals(Sech)) {
+				// JavaForm[2/(E^x+E^(-x))]
+				return Times(C2,Power(Plus(Power(E,x),Power(E,Times(CN1,x))),CN1));
+			}
+			if (head.equals(Sinh)) {
+				// JavaForm[(E^x-E^(-x))/2]
+				return Times(C1D2, Plus(Power(E, x), Times(CN1, Power(E, Times(CN1, x)))));
+			}
+			if (head.equals(Tanh)) {
+				// JavaForm[((-E^(-x))+E^x)/((E^(-x))+E^x)]
+				return Times(Plus(Times(CN1, Power(E, Times(CN1, x))), Power(E, x)),
+						Power(Plus(Power(E, Times(CN1, x)), Power(E, x)), CN1));
 			}
 			if (result != null) {
 				return $(head, result);
