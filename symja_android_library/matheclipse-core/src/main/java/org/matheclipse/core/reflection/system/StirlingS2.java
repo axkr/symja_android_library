5c660d43c1397572c342bf281925d4a21dec2a18
axexlck <axelclk@gmail.com>
2013-09-15 08:52:48 +0200

    Catch possible ArithmeticException in StirlingS2() function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/StirlingS2.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/StirlingS2.java
@@ -23,9 +23,8 @@ import org.matheclipse.parser.client.SyntaxError;
 /**
  * Stirling numbers of the second kind.
  * 
- * See <a
- * href="http://en.wikipedia.org/wiki/Stirling_numbers_of_the_second_kind"
- * >Wikipedia - Stirling numbers of the second kind</a>
+ * See <a href="http://en.wikipedia.org/wiki/Stirling_numbers_of_the_second_kind" >Wikipedia - Stirling numbers of the second
+ * kind</a>
  */
 public class StirlingS2 extends AbstractFunctionEvaluator {
 
@@ -36,31 +35,35 @@ public class StirlingS2 extends AbstractFunctionEvaluator {
 	@Override
 	public IExpr evaluate(final IAST ast) {
 		Validate.checkSize(ast, 3);
-		
-		if (ast.get(1).isInteger() && ast.get(2).isInteger()) {
-			if (ast.get(1).equals(ast.get(2))) {
-				// {n,n}==1
-				return C1;
-			}
-			IAST temp = F.Plus();
-			// int n = ((IInteger) ast.get(1)).toInt();
-			int k = ((IInteger) ast.get(2)).toInt();
-			if (k == 1) {
-				// {n,1}==1
-				return C1;
-			}
-			if (k == 2) {
-				// {n,2}==2^(n-1)-1
-				return Subtract(Power(C2, Subtract(ast.get(1), C1)), C1);
-			}
-			for (int j = 0; j < k; j++) {
-				if ((j & 1) == 1) {
-					temp.add(Times(Times(CN1, Binomial(ast.get(2), integer(j))), Power(Plus(ast.get(2), integer(-j)), ast.get(1))));
-				} else {
-					temp.add(Times(Times(Binomial(ast.get(2), integer(j))), Power(Plus(ast.get(2), integer(-j)), ast.get(1))));
+
+		try {
+			if (ast.get(1).isInteger() && ast.get(2).isInteger()) {
+				if (ast.get(1).equals(ast.get(2))) {
+					// {n,n}==1
+					return C1;
 				}
+				IAST temp = F.Plus();
+				int k = ((IInteger) ast.get(2)).toInt();
+				if (k == 1) {
+					// {n,1}==1
+					return C1;
+				}
+				if (k == 2) {
+					// {n,2}==2^(n-1)-1
+					return Subtract(Power(C2, Subtract(ast.get(1), C1)), C1);
+				}
+				for (int j = 0; j < k; j++) {
+					if ((j & 1) == 1) {
+						temp.add(Times(Times(CN1, Binomial(ast.get(2), integer(j))),
+								Power(Plus(ast.get(2), integer(-j)), ast.get(1))));
+					} else {
+						temp.add(Times(Times(Binomial(ast.get(2), integer(j))), Power(Plus(ast.get(2), integer(-j)), ast.get(1))));
+					}
+				}
+				return Times(Power(Factorial(ast.get(2)), CN1), temp);
 			}
-			return Times(Power(Factorial(ast.get(2)), CN1), temp);
+		} catch (ArithmeticException ae) {
+			// because of toInt() method
 		}
 		return null;
 	}
