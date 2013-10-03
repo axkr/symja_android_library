19f453f167024f6f42f5aa4d1755330a1e77c9a4
axexlck <axelclk@gmail.com>
2013-10-03 15:46:42 +0200

    clean up

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Max.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Max.java
@@ -1,5 +1,6 @@
 package org.matheclipse.core.reflection.system;
 
+import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.generic.ITernaryComparator.COMPARE_RESULT;
@@ -12,47 +13,46 @@ public class Max extends AbstractFunctionEvaluator {
 	}
 
 	public IExpr evaluate(final IAST ast) {
-		IAST list = ast;
-		if (list.size() > 1) {
-			IAST resultList = list.copyHead();
-			if (EvaluationSupport.flatten(F.List, list, resultList)) {
-				list = resultList;
-			}
-			// IExpr max = F.Times(F.CN1, ExprFactory.Infinity);
-			IExpr max1;
-			IExpr max2;
-			max1 = list.get(1);
-			IAST f = list.copyHead();
-			COMPARE_RESULT comp;
-			for (int i = 2; i < list.size(); i++) {
-				max2 = list.get(i);
-				comp = Less.CONST.prepareCompare(max1, max2);
+		Validate.checkRange(ast, 2);
 
-				if (comp == COMPARE_RESULT.TRUE) {
-					max1 = max2;
-				} else {
-					if (comp == COMPARE_RESULT.UNDEFINED) {
-						// undetermined
-						if (max1.isNumber()) {
-							f.add(max2);
-						} else {
-							f.add(max1);
-							max1 = max2;
-						}
+		IAST list = ast;
+		IAST resultList = list.copyHead();
+		if (EvaluationSupport.flatten(F.List, list, resultList)) {
+			list = resultList;
+		}
+		// IExpr max = F.Times(F.CN1, ExprFactory.Infinity);
+		IExpr max1;
+		IExpr max2;
+		max1 = list.get(1);
+		IAST f = list.copyHead();
+		COMPARE_RESULT comp;
+		for (int i = 2; i < list.size(); i++) {
+			max2 = list.get(i);
+			comp = Less.CONST.prepareCompare(max1, max2);
+
+			if (comp == COMPARE_RESULT.TRUE) {
+				max1 = max2;
+			} else {
+				if (comp == COMPARE_RESULT.UNDEFINED) {
+					// undetermined
+					if (max1.isNumber()) {
+						f.add(max2);
+					} else {
+						f.add(max1);
+						max1 = max2;
 					}
 				}
 			}
-			if (f.size() > 1) {
-				f.add(1, max1);
-				if (f.equals(list)) {
-					return null;
-				}
-				return f;
-			} else {
-				return max1;
-			}
 		}
-		return null;
+		if (f.size() > 1) {
+			f.add(1, max1);
+			if (f.equals(list)) {
+				return null;
+			}
+			return f;
+		} else {
+			return max1;
+		}
 	}
 
 }
