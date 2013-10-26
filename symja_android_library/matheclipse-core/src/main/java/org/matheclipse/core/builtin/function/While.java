27e3422526c8a151445b4ad764d373ad8d834d47
axexlck <axelclk@gmail.com>
2013-10-26 18:33:38 +0200

    Improved While() function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/While.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/While.java
@@ -19,16 +19,23 @@ public class While extends AbstractCoreFunctionEvaluator {
 
 	@Override
 	public IExpr evaluate(final IAST ast) {
-		Validate.checkSize(ast, 3);
+		Validate.checkRange(ast, 2, 3);
 		final EvalEngine engine = EvalEngine.get();
 
 		final int iterationLimit = engine.getIterationLimit();
 		int iterationCounter = 1;
-
-		while (engine.evaluate(ast.get(1)).equals(F.True)) {
+		// While(test, body)
+		IExpr test = ast.get(1);
+		IExpr body = F.Null;
+		if (ast.size() == 3) {
+			body = ast.get(2);
+		}
+		
+		while (engine.evaluate(test).isTrue()) {
 			try {
-				engine.evaluate(ast.get(2));
-
+				if (ast.size() == 3) {
+					engine.evaluate(body);
+				}
 				if (iterationLimit >= 0 && iterationLimit <= ++iterationCounter) {
 					IterationLimitExceeded.throwIt(iterationCounter, ast);
 				}
