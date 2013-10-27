1f7af672e906d05b601b7960b2304331b7e56ad5
axexlck <axelclk@gmail.com>
2013-10-27 11:41:39 +0100

    MemberQ

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/MemberQ.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/MemberQ.java
@@ -2,6 +2,7 @@ package org.matheclipse.core.builtin.function;
 
 import org.matheclipse.core.eval.exception.Validate;
 import org.matheclipse.core.eval.interfaces.AbstractCoreFunctionEvaluator;
+import org.matheclipse.core.eval.util.Options;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
@@ -14,11 +15,22 @@ public class MemberQ extends AbstractCoreFunctionEvaluator {
 	}
 
 	public IExpr evaluate(final IAST ast) {
-		Validate.checkSize(ast, 3);
+		Validate.checkRange(ast, 3, 5);
 
+		boolean heads = false;
+		if (ast.size() > 3) {
+			final Options options = new Options(ast.topHead(), ast, ast.size() - 1);
+			IExpr option = options.getOption("Heads");
+			if (option.isTrue()) {
+				heads = true;
+			}
+		}
 		final IExpr arg1 = F.eval(ast.get(1));
 		final IExpr arg2 = F.eval(ast.get(2));
-		return F.bool(arg1.isMember(arg2, false));
+		if (arg1.isAST()) {
+			return F.bool(arg1.isMember(arg2, heads));
+		}
+		return F.False;
 	}
 
 }
