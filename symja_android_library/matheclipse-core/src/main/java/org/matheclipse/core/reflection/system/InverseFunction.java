71f9a45354af83239b4552b108d4bd80e4604b24
axexlck <axelclk@gmail.com>
2013-11-21 19:27:17 +0100

    Add more inverse functions

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/InverseFunction.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/InverseFunction.java
@@ -14,22 +14,34 @@ import org.matheclipse.core.interfaces.ISymbol;
  *  
  */
 public class InverseFunction extends AbstractFunctionEvaluator {
-	private static Map<IExpr, IExpr> INVERSE_FUNCTIONS = new HashMap<IExpr, IExpr>();
+	private static Map<ISymbol, ISymbol> INVERSE_FUNCTIONS = new HashMap<ISymbol, ISymbol>();
 
 	static {
 		INVERSE_FUNCTIONS.put(F.Cos, F.ArcCos);
+		INVERSE_FUNCTIONS.put(F.Cot, F.ArcCot);
+		INVERSE_FUNCTIONS.put(F.Csc, F.ArcCsc);
+		INVERSE_FUNCTIONS.put(F.Sec, F.ArcSec);
 		INVERSE_FUNCTIONS.put(F.Sin, F.ArcSin);
 		INVERSE_FUNCTIONS.put(F.Tan, F.ArcTan);
-		
+
 		INVERSE_FUNCTIONS.put(F.ArcCos, F.Cos);
+		INVERSE_FUNCTIONS.put(F.ArcCot, F.Cot);
+		INVERSE_FUNCTIONS.put(F.ArcCsc, F.Csc);
+		INVERSE_FUNCTIONS.put(F.ArcSec, F.Sec);
 		INVERSE_FUNCTIONS.put(F.ArcSin, F.Sin);
 		INVERSE_FUNCTIONS.put(F.ArcTan, F.Tan);
-		
+
 		INVERSE_FUNCTIONS.put(F.Cosh, F.ArcCosh);
+		INVERSE_FUNCTIONS.put(F.Coth, F.ArcCoth);
+		INVERSE_FUNCTIONS.put(F.Csch, F.ArcCsch);
+		INVERSE_FUNCTIONS.put(F.Sech, F.ArcSech);
 		INVERSE_FUNCTIONS.put(F.Sinh, F.ArcSinh);
 		INVERSE_FUNCTIONS.put(F.Tanh, F.ArcTanh);
-		
+
 		INVERSE_FUNCTIONS.put(F.ArcCosh, F.Cosh);
+		INVERSE_FUNCTIONS.put(F.ArcCoth, F.Coth);
+		INVERSE_FUNCTIONS.put(F.ArcCsch, F.Csch);
+		INVERSE_FUNCTIONS.put(F.ArcSech, F.Sech);
 		INVERSE_FUNCTIONS.put(F.ArcSinh, F.Sinh);
 		INVERSE_FUNCTIONS.put(F.ArcTanh, F.Tanh);
 	}
@@ -41,11 +53,36 @@ public class InverseFunction extends AbstractFunctionEvaluator {
 	@Override
 	public IExpr evaluate(final IAST ast) {
 		Validate.checkSize(ast, 2);
+		ISymbol arg1 = Validate.checkSymbolType(ast, 1);
+		return getInverseFunction(arg1);
+	}
 
-		IExpr arg1 = ast.arg1();
-		IExpr inverseFunction = INVERSE_FUNCTIONS.get(arg1);
-		if (inverseFunction != null) {
-			return inverseFunction;
+	/**
+	 * Get the inverse function symbol if possible.
+	 * 
+	 * @param headSymbol
+	 *            the symbol which represents a function name (i.e. <code>Cos, Sin, ArcSin,...</code>)
+	 * @return <code>null</code> if there is no inverse function defined.
+	 */
+	public static ISymbol getInverseFunction(ISymbol headSymbol) {
+		return INVERSE_FUNCTIONS.get(headSymbol);
+	}
+
+	/**
+	 * Get a new constructed inverse function AST from the given <code>ast</code>, with empty arguments (i.e.
+	 * <code>inverseAST.size()==1)</code>.
+	 * 
+	 * @param ast
+	 *            the AST which represents a function (i.e. <code>Cos(x), Sin(x), ArcSin(x),...</code>)
+	 * @return <code>null</code> if there is no inverse function defined.
+	 */
+	public static IAST getInverseFunction(IAST ast) {
+		IExpr expr = ast.head();
+		if (expr.isSymbol()) {
+			ISymbol inverseSymbol = INVERSE_FUNCTIONS.get((ISymbol) expr);
+			if (inverseSymbol != null) {
+				return F.ast(inverseSymbol);
+			}
 		}
 		return null;
 	}
@@ -53,5 +90,5 @@ public class InverseFunction extends AbstractFunctionEvaluator {
 	public void setUp(final ISymbol symbol) {
 		symbol.setAttributes(ISymbol.NHOLDALL);
 	}
-	
+
 }
