4afe5b039e304e930d774b22bc00adc1a16e6f19
axexlck <axelclk@gmail.com>
2013-11-24 18:11:48 +0100

    Not() function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Not.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Not.java
@@ -18,10 +18,40 @@ public class Not extends AbstractArg1 {
 		if (o.isFalse()) {
 			return F.True;
 		}
-		if (o.isNot()) {
-			return ((IAST) o).arg1();
+		if (o.isAST()) {
+			IAST temp = (IAST) o;
+			if (o.isNot()) {
+				return ((IAST) o).arg1();
+			}
+			if (temp.size() == 3) {
+				IExpr head = temp.head();
+				if (head.equals(F.Equal)) {
+					temp = temp.clone();
+					temp.set(0, F.Unequal);
+					return temp;
+				} else if (head.equals(F.Unequal)) {
+					temp = temp.clone();
+					temp.set(0, F.Equal);
+					return temp;
+				} else if (head.equals(F.Greater)) {
+					temp = temp.clone();
+					temp.set(0, F.LessEqual);
+					return temp;
+				} else if (head.equals(F.GreaterEqual)) {
+					temp = temp.clone();
+					temp.set(0, F.Less);
+					return temp;
+				} else if (head.equals(F.Less)) {
+					temp = temp.clone();
+					temp.set(0, F.GreaterEqual);
+					return temp;
+				} else if (head.equals(F.LessEqual)) {
+					temp = temp.clone();
+					temp.set(0, F.Greater);
+					return temp;
+				}
+			}
 		}
-		 
 		return null;
 	}
 
