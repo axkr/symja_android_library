d9dd07986ad32c98812bc0cb38a611539d06f2d4
axexlck <axelclk@gmail.com>
2013-11-25 18:38:08 +0100

    Improved ContinuedFraction() function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/ContinuedFraction.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/ContinuedFraction.java
@@ -1,7 +1,8 @@
 package org.matheclipse.core.reflection.system;
 
+import java.math.BigInteger;
+
 import org.matheclipse.core.eval.exception.Validate;
-import org.matheclipse.core.eval.exception.WrongNumberOfArguments;
 import org.matheclipse.core.eval.interfaces.IFunctionEvaluator;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
@@ -11,13 +12,10 @@ import org.matheclipse.core.interfaces.INum;
 import org.matheclipse.core.interfaces.IRational;
 import org.matheclipse.core.interfaces.ISymbol;
 
-import java.math.BigInteger;
-
 /**
  * ContinuedFraction of a double or fraction number
  * 
- * See: <a href="http://en.wikipedia.org/wiki/Continued_fraction">Continued
- * fraction</a>
+ * See: <a href="http://en.wikipedia.org/wiki/Continued_fraction">Continued fraction</a>
  * 
  * @see org.matheclipse.core.reflection.system.FromContinuedFraction
  */
@@ -27,12 +25,24 @@ public class ContinuedFraction implements IFunctionEvaluator {
 	}
 
 	public IExpr evaluate(final IAST ast) {
-		Validate.checkSize(ast, 2);
+		Validate.checkRange(ast, 2, 3);
 
 		IExpr arg1 = ast.arg1();
+		
+		int maxIterations = Integer.MAX_VALUE;
+		if (ast.size() == 3 && ast.arg2().isInteger()) {
+			maxIterations = Validate.checkIntType(ast, 2);
+		}
+		
 		if (arg1 instanceof INum) {
 			arg1 = F.fraction(((INum) arg1).getRealPart());
+		} else if (arg1.isAST() || arg1.isSymbol() && arg1.isNumericFunction()) {
+			IExpr num = F.evaln(arg1);
+			if (num instanceof INum) {
+				arg1 = F.fraction(((INum) num).getRealPart());
+			}
 		}
+
 		if (arg1.isRational()) {
 			IRational rat = (IRational) arg1;
 
@@ -46,7 +56,7 @@ public class ContinuedFraction implements IFunctionEvaluator {
 				IFraction temp = F.fraction(rat.getNumerator(), rat.getDenominator());
 				BigInteger quotient;
 				BigInteger remainder;
-				while (temp.getDenominator().isGreaterThan(F.C1)) {
+				while (temp.getDenominator().isGreaterThan(F.C1) && (0 < maxIterations--)) {
 					quotient = temp.getBigNumerator().divide(temp.getBigDenominator());
 					remainder = temp.getBigNumerator().mod(temp.getBigDenominator());
 					continuedFractionList.add(F.integer(quotient));
