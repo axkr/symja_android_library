c26339ebd3ab165cc51e283bd262d87f64cc6969
axexlck <axelclk@gmail.com>
2013-12-01 12:16:24 +0100

    Simplify AddTo() function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/AddTo.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/AddTo.java
@@ -1,8 +1,9 @@
 package org.matheclipse.core.reflection.system;
 
-import org.matheclipse.core.eval.EvalEngine;
-import org.matheclipse.core.eval.interfaces.AbstractArg2;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
 
@@ -12,7 +13,7 @@ import com.google.common.base.Function;
  * Operator +=
  * 
  */
-public class AddTo extends AbstractArg2 {
+public class AddTo extends AbstractFunctionEvaluator {
 	class AddToFunction implements Function<IExpr, IExpr> {
 		final IExpr value;
 
@@ -27,6 +28,13 @@ public class AddTo extends AbstractArg2 {
 
 	}
 
+	/**
+	 * Get the evaluation function for AddTo (similar for DivideBy, TimesBy and SubtractFrom).
+	 * 
+	 * @param value
+	 *            the value which is assigned in this statement (i.e. <code>variable += value</code>)
+	 * @return
+	 */
 	protected Function<IExpr, IExpr> getFunction(IExpr value) {
 		return new AddToFunction(value);
 	}
@@ -40,21 +48,17 @@ public class AddTo extends AbstractArg2 {
 	}
 
 	@Override
-	public IExpr e2ObjArg(final IExpr o0, final IExpr o1) {
-		final EvalEngine engine = EvalEngine.get();
-		if (o0.isSymbol()) {
-			final IExpr v1 = engine.evaluate(o1);
-			final ISymbol sym = (ISymbol) o0;
-
-			IExpr[] results = sym.reassignSymbolValue(getFunction(v1), getFunctionSymbol());
-			if (results != null) {
-				return results[1];
-			}
-		}
-
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkSize(ast, 3);
+		ISymbol sym = Validate.checkSymbolType(ast, 1);
+		IExpr arg2 = F.eval(ast.arg2());
+		IExpr[] results = ((ISymbol) sym).reassignSymbolValue(getFunction(F.eval(arg2)), getFunctionSymbol());
+		if (results != null) {
+			return results[1];
+		} 
 		return null;
 	}
-
+	
 	@Override
 	public void setUp(final ISymbol symbol) {
 		symbol.setAttributes(ISymbol.HOLDALL);
