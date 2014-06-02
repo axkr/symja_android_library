86c4aed39fa3befc35588bc4f53b264c6924b271
axexlck <axelclk@gmail.com>
2014-06-02 22:06:32 +0200

    More Limit() rules

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/LimitRules.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/LimitRules.java
@@ -5,10 +5,16 @@ import org.matheclipse.core.interfaces.IAST;
 
 public interface LimitRules {
 	final public static IAST RULES = List(
-SetDelayed(Limit(Power(x_,$p(m,IntegerQ)),Rule(x_Symbol,CInfinity)),
+SetDelayed(Limit(Power(x_,$p(m,NumberQ)),Rule(x_Symbol,CInfinity)),
+    Condition(CInfinity,Positive(m))),
+SetDelayed(Limit(Power(x_,$p(m,NumberQ)),Rule(x_Symbol,CInfinity)),
     Condition(C0,Negative(m))),
-SetDelayed(Limit(Power(x_,$p(m,IntegerQ)),Rule(x_Symbol,CNInfinity)),
-    Condition(C0,Negative(m))),
+SetDelayed(Limit(Power($p(m,NumberQ),x_),Rule(x_Symbol,CInfinity)),
+    Condition(If(Greater(m,C1),CInfinity,If(Equal(m,C1),C1,C0)),Positive(m))),
+SetDelayed(Limit(Power($p(m,NumberQ),Times(CN1,x_)),Rule(x_Symbol,CInfinity)),
+    Condition(C0,Greater(m,C1))),
+Set(Limit(Log(x_),Rule(x_Symbol,CInfinity)),
+    CInfinity),
 Set(Limit(Power(Plus(C1,Power(x_,CN1)),x_),Rule(x_Symbol,CInfinity)),
     E),
 Set(Limit(Power(Plus(C1,Times(Power(x_,CN1),a_)),x_),Rule(x_Symbol,CInfinity)),
--- symja_android_library/rules/LimitRules.m
+++ symja_android_library/rules/LimitRules.m
@@ -1,6 +1,9 @@
 {
- Limit(x_^m_IntegerQ, x_Symbol->Infinity):= 0 /; Negative(m),
- Limit(x_^m_IntegerQ, x_Symbol->DirectedInfinity(-1)):= 0 /; Negative(m),
+ Limit(x_^m_NumberQ, x_Symbol->Infinity):= Infinity /; Positive(m),
+ Limit(x_^m_NumberQ, x_Symbol->Infinity):= 0 /; Negative(m),
+ Limit(m_NumberQ^x_, x_Symbol->Infinity):= If(m>1, Infinity, If(m==1, 1, 0)) /; Positive(m),
+ Limit(m_NumberQ^(-x_), x_Symbol->Infinity):= 0 /; m>1,
+ Limit(Log(x_), x_Symbol->Infinity)=Infinity,
  Limit((1+x_^(-1))^x_, x_Symbol->Infinity)=E,
  Limit((1+a_*(x_^(-1)))^x_, x_Symbol->Infinity)=E^(a) /; FreeQ(a,x), 
  Limit(Sum(y_Symbol^(s_IntegerQ),{y_Symbol,1,x_Symbol}), x_Symbol->Infinity):=Module({v=-s/2},((2*Pi)^(2*v)*(-1)^(v+1)*BernoulliB(2*v))/(2*(2*v)!)) /; EvenQ(s)&&s<0
