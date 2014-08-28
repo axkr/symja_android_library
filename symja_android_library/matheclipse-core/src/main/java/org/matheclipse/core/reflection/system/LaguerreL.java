068007fdd72adfb5a94a07d9d74328d9578b18b2
axexlck <axelclk@gmail.com>
2014-08-28 20:47:54 +0200

    ChebyshevT(), HermiteH(), LaguerreL(), LegendreP()

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/LaguerreL.java
@@ -0,0 +1,22 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.polynomials.PolynomialsUtils;
+
+public class LaguerreL extends AbstractFunctionEvaluator {
+
+	public LaguerreL() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkSize(ast, 3);
+		int degree = Validate.checkIntType(ast.arg1());
+
+		return PolynomialsUtils.createLaguerrePolynomial(degree, ast.arg2());
+	}
+
+}
---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/LegendreP.java
@@ -0,0 +1,22 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.polynomials.PolynomialsUtils;
+
+public class LegendreP extends AbstractFunctionEvaluator {
+
+	public LegendreP() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkSize(ast, 3);
+		int degree = Validate.checkIntType(ast.arg1());
+		
+		return PolynomialsUtils.createLegendrePolynomial(degree, ast.arg2());
+	}
+
+}
