4f8c62ebe6d3f2dfdf8c8ec27e504a14687f7fe4
axexlck <axelclk@gmail.com>
2014-10-12 17:07:47 +0200

    UnitStep() implemented INumeric interface

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/UnitStep.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/UnitStep.java
@@ -1,6 +1,7 @@
 package org.matheclipse.core.reflection.system;
 
 import org.matheclipse.core.eval.interfaces.IFunctionEvaluator;
+import org.matheclipse.core.eval.interfaces.INumeric;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
@@ -10,24 +11,35 @@ import org.matheclipse.core.interfaces.ISymbol;
 /**
  * 
  */
-public class UnitStep implements IFunctionEvaluator {
+public class UnitStep implements INumeric, IFunctionEvaluator {
 
 	public UnitStep() {
+	}
+
+	@Override
+	public double evalReal(double[] stack, int top, int size) {
+		for (int i = top - size + 1; i < top + 1; i++) {
+			if (stack[i] < 0.0) {
+				return 0.0;
+			}
+		}
+		return 1.0;
 	}
 
 	public IExpr evaluate(final IAST ast) {
 		int size = ast.size();
 		if (size > 1) {
 			for (int i = 1; i < size; i++) {
-				if (ast.get(i).isSignedNumber()) {
-					if (((ISignedNumber) ast.get(i)).complexSign() < 0) {
+				IExpr temp = ast.get(i);
+				if (temp.isSignedNumber()) {
+					if (((ISignedNumber) temp).complexSign() < 0) {
 						return F.C0;
 					} else {
 						continue;
 					}
 				}
-				if (ast.get(i).isNumericFunction()) {
-					IExpr num = F.evaln(ast.get(i));
+				if (temp.isNumericFunction()) {
+					IExpr num = F.evaln(temp);
 					if (num.isSignedNumber()) {
 						if (((ISignedNumber) num).complexSign() < 0) {
 							return F.C0;
@@ -49,5 +61,4 @@ public class UnitStep implements IFunctionEvaluator {
 	public void setUp(ISymbol symbol) {
 		symbol.setAttributes(ISymbol.HOLDALL | ISymbol.ORDERLESS | ISymbol.LISTABLE);
 	}
-
 }
