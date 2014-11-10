25f1bdac76bfb2ba99b90ea1d0a03c21eeaa9b8a
axexlck <axelclk@gmail.com>
2014-11-10 21:01:39 +0100

    Deleted AbstractHashedPatternRules#equals()

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/patternmatching/AbstractHashedPatternRules.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/patternmatching/AbstractHashedPatternRules.java
@@ -55,38 +55,6 @@ public abstract class AbstractHashedPatternRules {
 		return 31 * (h1 + h2);
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj instanceof HashedPatternRules) {
-			HashedPatternRules other = (HashedPatternRules) obj;
-			if (hash1 != other.hash1) {
-				return false;
-			}
-			if (hash2 != other.hash2) {
-				return false;
-			}
-			if (fLHSPattern1 == null) {
-				if (other.fLHSPattern1 != null) {
-					return false;
-				}
-			} else if (!fLHSPattern1.equals(other.fLHSPattern1)) {
-				return false;
-			}
-			if (fLHSPattern2 == null) {
-				if (other.fLHSPattern2 != null) {
-					return false;
-				}
-			} else if (!fLHSPattern2.equals(other.fLHSPattern2)) {
-				return false;
-			}
-			return true;
-		}
-		return false;
-	}
-
 	/**
 	 * Get the hash value for the first LHS expression.
 	 * 
