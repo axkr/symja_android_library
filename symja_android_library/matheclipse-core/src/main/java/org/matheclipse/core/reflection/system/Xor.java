a1c21a84e5f7c5be39f10c55b426c3e131213633
axexlck <axelclk@gmail.com>
2014-11-24 18:59:03 +0100

    Xor() function

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Xor.java
@@ -0,0 +1,42 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class Xor extends AbstractFunctionEvaluator {
+	public Xor() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkRange(ast, 3);
+
+		boolean result = false;
+
+		for (int i = 1; i < ast.size(); i++) {
+			if (ast.get(i).isTrue()) {
+				result = !result;
+			} else if (ast.get(i).isFalse()) {
+			} else {
+				return null;
+			}
+		} 
+
+		return F.bool(result);
+	}
+
+	@Override
+	public void setUp(final ISymbol symbol) {
+		symbol.setAttributes(ISymbol.ORDERLESS | ISymbol.ONEIDENTITY | ISymbol.FLAT);
+	}
+}
