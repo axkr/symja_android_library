fb2fc76e5a0df8b9bf61e757240d677119c462ad
axexlck <axelclk@gmail.com>
2014-11-29 15:05:11 +0100

    SumRules

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/SumRules.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/rules/SumRules.java
@@ -9,7 +9,9 @@ import org.matheclipse.core.interfaces.IAST;
  */
 public interface SumRules {
   final public static IAST RULES = List(
-    ISetDelayed(Sum(Ceiling(Log(a_,$p(i))),List($p(i, SymbolHead),C1,n_)),
+    ISetDelayed(Sum(Ceiling(Log($p(i))),List($p(i, SymbolHead),C1,$p(n, SymbolHead))),
+      Condition(Plus(Times(Plus(Times(Floor(Log(n)),Power(E,Plus(Floor(Log(n)),C1))),Times(CN1,Plus(Floor(Log(n)),C1),Power(E,Floor(Log(n)))),C1),Power(Plus(E,Times(CN1,C1)),CN1)),Times(Plus(n,Times(CN1,Power(E,Floor(Log(n))))),Ceiling(Log(n)))),FreeQ(n,i))),
+    ISetDelayed(Sum(Ceiling(Log(a_,$p(i))),List($p(i, SymbolHead),C1,$p(n, SymbolHead))),
       Condition(Plus(Times(Plus(Times(Floor(Log(a,n)),Power(a,Plus(Floor(Log(a,n)),C1))),Times(CN1,Plus(Floor(Log(a,n)),C1),Power(a,Floor(Log(a,n)))),C1),Power(Plus(a,Times(CN1,C1)),CN1)),Times(Plus(n,Times(CN1,Power(a,Floor(Log(a,n))))),Ceiling(Log(a,n)))),And(FreeQ(a,i),FreeQ(n,i))))
   );
 }
--- symja_android_library/rules/SumRules.m
+++ symja_android_library/rules/SumRules.m
@@ -1,5 +1,8 @@
  {
-Sum(Ceiling(Log(a_,i_)), {i_Symbol,1,n_}):=
+ Sum(Ceiling(Log(i_)), {i_Symbol,1,n_Symbol}):=
+  ( Floor(Log(n))*E^(Floor(Log(n))+1)-(Floor(Log(n))+1)*E^Floor(Log(n))+1 ) * (E-1)^(-1) + (n-E^Floor(Log(n)))*Ceiling(Log(n))
+  /; FreeQ(n,i),
+Sum(Ceiling(Log(a_,i_)), {i_Symbol,1,n_Symbol}):=
   ( Floor(Log(a,n))*a^(Floor(Log(a,n))+1)-(Floor(Log(a,n))+1)*a^Floor(Log(a,n))+1 ) * (a-1)^(-1) + (n-a^Floor(Log(a,n)))*Ceiling(Log(a,n))
   /; FreeQ(a,i)&&FreeQ(n,i)
 }
