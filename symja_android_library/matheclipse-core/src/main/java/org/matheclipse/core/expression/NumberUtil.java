d80b5f1099a24fb0e4855848b6afc830a8b50b65
axexlck <axelclk@gmail.com>
2014-12-07 17:28:41 +0100

    Added NumberUtil#convertToFraction() method

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/expression/NumberUtil.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/expression/NumberUtil.java
@@ -511,4 +511,37 @@ public class NumberUtil {
 		return false;
 	}
 
+	/**
+	 * Convert a <code>double</code> number into a reduced ratio of two BigIntegers that mathematically represents exactly the same
+	 * value.
+	 * 
+	 * See: <a href="http://stackoverflow.com/questions/27259162">http://stackoverflow.com/questions/27259162</a>
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static BigInteger[] convertToFraction(double value) {
+		int exponent = Math.getExponent(value);
+		if (exponent > Double.MAX_EXPONENT) {
+			// The value is infinite or NaN.
+			throw new IllegalArgumentException("Illegal parameter 'value': " + value);
+		}
+		long positiveSignificand;
+		if (exponent < Double.MIN_EXPONENT) {
+			// The value is subnormal.
+			exponent++;
+			positiveSignificand = Double.doubleToLongBits(value) & 0x000fffffffffffffL;
+		} else {
+			positiveSignificand = (Double.doubleToLongBits(value) & 0x000fffffffffffffL) | 0x0010000000000000L;
+		}
+		BigInteger significand = BigInteger.valueOf(value < 0 ? -positiveSignificand : positiveSignificand);
+		exponent -= 52; // Adjust the exponent for an integral significand.
+		BigInteger coefficient = BigInteger.ONE.shiftLeft(Math.abs(exponent));
+		if (exponent >= 0) {
+			return new BigInteger[] { significand.multiply(coefficient), BigInteger.ONE };
+		} else {
+			BigInteger gcd = significand.gcd(coefficient);
+			return new BigInteger[] { significand.divide(gcd), coefficient.divide(gcd) };
+		}
+	}
 }
