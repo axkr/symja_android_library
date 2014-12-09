b73268a72d908a5f1720403a09d2af530efec10a
axexlck <axelclk@gmail.com>
2014-12-09 21:08:29 +0100

    Cancel() clean up

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Cancel.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Cancel.java
@@ -11,9 +11,7 @@ import org.matheclipse.core.expression.ExprRingFactory;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
-import org.matheclipse.core.interfaces.IInteger;
 import org.matheclipse.core.interfaces.ISymbol;
-import org.matheclipse.core.polynomials.ExponentArray;
 import org.matheclipse.core.polynomials.Polynomial;
 import org.matheclipse.parser.client.SyntaxError;
 
@@ -27,15 +25,15 @@ import edu.jas.ufd.GreatestCommonDivisor;
  * Cancel(expr) - cancels out common factors in numerators and denominators.
  */
 public class Cancel extends AbstractFunctionEvaluator {
+
 	/**
 	 * This predicate identifies polynomial expressions. It requires that the given expression is already expanded for
 	 * <code>Plus,Power and Times</code> operations.
-	 * 
 	 */
 	private final static class PolynomialPredicate implements Predicate<IExpr> {
 
 		public boolean apply(IExpr expr) {
-			return expr.isPolynomial( F.List());
+			return expr.isPolynomial(F.List());
 		}
 	}
 
@@ -48,20 +46,12 @@ public class Cancel extends AbstractFunctionEvaluator {
 
 		IExpr arg1 = ast.arg1();
 		if (arg1.isPlus()) {
-			// IAST result = ((IAST) arg1).map(Functors.replaceArg(F.Cancel(F.Slot1), 1));
-			IAST result = ((IAST) arg1).mapAt(F.Cancel(null), 1);
-			if (result != null) {
-				return result;
-			}
+			return ((IAST) arg1).mapAt(F.Cancel(null), 1);
 		}
 		IExpr expandedArg1 = F.evalExpandAll(arg1);
 		try {
 			if (expandedArg1.isPlus()) {
-				// IAST result = ((IAST) expandedArg1).map(Functors.replaceArg(F.Cancel(F.Slot1), 1));
-				IAST result = ((IAST) expandedArg1).mapAt(F.Cancel(null), 1);
-				if (result != null) {
-					return result;
-				}
+				return ((IAST) expandedArg1).mapAt(F.Cancel(null), 1);
 			} else if (expandedArg1.isTimes() || expandedArg1.isPower()) {
 				IExpr result = cancelPowerTimes(expandedArg1);
 				if (result != null) {
@@ -102,7 +92,7 @@ public class Cancel extends AbstractFunctionEvaluator {
 	}
 
 	/**
-	 * Calculate the 3 element result array
+	 * Calculate the 3 elements result array
 	 * <code>[ commonFactor, poly1.divide(gcd(poly1, poly2)), poly2.divide(gcd(poly1, poly2)) ]</code> for the given expressions
 	 * <code>poly1</code> and <code>poly2</code>.
 	 * 
@@ -142,14 +132,6 @@ public class Cancel extends AbstractFunctionEvaluator {
 					result[0] = F.C1;
 					result[1] = F.eval(jas.exprPoly2Expr(p1.divide(gcd)));
 					result[2] = F.eval(jas.exprPoly2Expr(p2.divide(gcd)));
-					// java.math.BigInteger commonNumerator, commonDenominator;
-					// Object[] objects = jas.factorTerms(p1.divide(gcd));
-					// commonNumerator = (java.math.BigInteger) objects[0];
-					// result[1] = jas.integerPoly2Expr((GenPolynomial<BigInteger>) objects[2]);
-					// objects = jas.factorTerms(p2.divide(gcd));
-					// commonDenominator = (java.math.BigInteger) objects[0];
-					// result[2] = jas.integerPoly2Expr((GenPolynomial<BigInteger>) objects[2]);
-					// result[0] = F.fraction(commonNumerator, commonDenominator);
 				}
 				return result;
 			}
@@ -159,41 +141,6 @@ public class Cancel extends AbstractFunctionEvaluator {
 			}
 		}
 		return null;
-		// try {
-		// ExprVariables eVar = new ExprVariables(poly1);
-		// eVar.addVarList(poly2);
-		//
-		// ASTRange r = new ASTRange(eVar.getVarList(), 1);
-		// JASConvert<BigRational> jas = new JASConvert<BigRational>(r.toList(), BigRational.ZERO);
-		// GenPolynomial<BigRational> p1 = jas.expr2JAS(poly1, false);
-		// GenPolynomial<BigRational> p2 = jas.expr2JAS(poly2, false);
-		//
-		// BigRational cofac = new BigRational();
-		// GreatestCommonDivisor<BigRational> engine;
-		// engine = GCDFactory.getImplementation(cofac);
-		// GenPolynomial<BigRational> gcd = engine.gcd(p1, p2);
-		// IExpr[] result = new IExpr[3];
-		// if (gcd.isONE()) {
-		// result[0] = jas.rationalPoly2Expr(gcd);
-		// result[1] = jas.rationalPoly2Expr(p1);
-		// result[2] = jas.rationalPoly2Expr(p2);
-		// } else {
-		// java.math.BigInteger commonNumerator, commonDenominator;
-		// Object[] objects = jas.factorTerms(p1.divide(gcd));
-		// commonNumerator = (java.math.BigInteger) objects[0];
-		// result[1] = jas.integerPoly2Expr((GenPolynomial<BigInteger>) objects[2]);
-		// objects = jas.factorTerms(p2.divide(gcd));
-		// commonDenominator = (java.math.BigInteger) objects[0];
-		// result[2] = jas.integerPoly2Expr((GenPolynomial<BigInteger>) objects[2]);
-		// result[0] = F.fraction(commonNumerator, commonDenominator);
-		// }
-		// return result;
-		// } catch (Exception e) {
-		// if (Config.SHOW_STACKTRACE) {
-		// e.printStackTrace();
-		// }
-		// }
-		// return null;
 	}
 
 	@Override
