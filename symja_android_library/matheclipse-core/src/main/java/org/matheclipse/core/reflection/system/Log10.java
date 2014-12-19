a773461709ab7484d687c77521906481280d7d7a
jim bob <jimmmaaay@hotmail.com>
2014-12-19 05:50:38 +0000

    Wrong algorithm that cause stack overflow

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Log10.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Log10.java
@@ -11,7 +11,7 @@ public class Log10 extends AbstractArg1 implements INumeric {
 
 	@Override
 	public IExpr e1ObjArg(final IExpr o) {
-		return F.Divide(F.Log(o), F.Log(F.C10));
+		return Log(o,C10);//return F.Divide(F.Log(o), F.Log(F.C10));
 	}
 
 	public double evalReal(final double[] stack, final int top, final int size) {
