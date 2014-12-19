5ab5203ceffb7562e49b63b1013dc5ed58e7e067
jim bob <jimmmaaay@hotmail.com>
2014-12-19 05:46:27 +0000

    Surd.java created online with Bitbucket

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Surd.java
@@ -0,0 +1,20 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.convert.ExprVariables;
+import org.matheclipse.core.eval.interfaces.AbstractArg2;
+import org.matheclipse.core.eval.interfaces.INumeric;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.expression.FractionSym;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+
+import static org.matheclipse.core.expression.F.Power;
+
+public class Surd extends AbstractArg2 implements INumeric {
+	@Override public IExpr e2ObjArg(final IExpr o,final IExpr r) {return Power(o,FractionSym.valueOf(1,Long.parseLong(r.toString())));}
+	@Override public void setUp(final ISymbol symbol) {
+		symbol.setAttributes(ISymbol.LISTABLE | ISymbol.NUMERICFUNCTION);
+		super.setUp(symbol);
+	}
+}
