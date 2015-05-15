74db9b206fc6169b1b35470637426710d948aa4a
axexlck <axelclk@gmail.com>
2015-05-15 10:03:42 +0200

    Constructor for evaluaton engine with history of the last XXX results

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/ExprEvaluator.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/ExprEvaluator.java
@@ -21,13 +21,36 @@ public class ExprEvaluator {
 	private Map<ISymbol, IExpr> fVariableMap;
 	private final List<ISymbol> fVariables;
 	private final EvalEngine engine;
-	
+
 	private IExpr fExpr;
 
+	/**
+	 * Constructor for an <code>IExpr</code> object evaluator. By default no output history for the <code>Out()</code> function is
+	 * stored in the evaluation engine. <code>$ans</code> won't get evaluate to the last result.
+	 * 
+	 */
 	public ExprEvaluator() {
+		this(true, 0);
+	}
+
+	/**
+	 * Constructor for an <code>IExpr</code> object evaluator. By default no output history for the <code>Out()</code> function is
+	 * stored in the evaluation engine. <code>$ans</code> won't get evaluate to the last result.
+	 * 
+	 * @param outListDisabled
+	 *            if <code>false</code> create a <code>LastCalculationsHistory(historyCapacity)</code>, otherwise no history of the
+	 *            last calculations will be saved and the <code>Out()</code> function (or <code>$ans</code> variable or the
+	 *            <code>%</code> operator) will be unevaluated.
+	 * @param historyCapacity
+	 *            the number of last entries of the calculations which should be stored.
+	 */
+	public ExprEvaluator(boolean outListDisabled, int historyCapacity) {
 		fVariableMap = new HashMap<ISymbol, IExpr>();
 		fVariables = new ArrayList<ISymbol>();
-		engine = new EvalEngine(true);
+		engine = new EvalEngine(true, outListDisabled);
+		if (!outListDisabled) {
+			engine.setOutListDisabled(outListDisabled, 100);
+		}
 	}
 
 	/**
@@ -142,8 +165,11 @@ public class ExprEvaluator {
 	public IExpr evaluate(final IExpr expr) {
 		fExpr = expr;
 		engine.reset();
-		return engine.evaluate(expr);
-
+		IExpr temp = engine.evaluate(expr);
+		if (!engine.isOutListDisabled()) {
+			engine.addOut(temp);
+		}
+		return temp;
 	}
 
 	/**
@@ -179,7 +205,7 @@ public class ExprEvaluator {
 	public IExpr getVariable(String variableName) {
 		return fVariableMap.get(F.$s(variableName));
 	}
-	
+
 	/**
 	 * Converts the inputExpression string into a MathML expression and writes the result to the given <code>Writer</code>
 	 * 
