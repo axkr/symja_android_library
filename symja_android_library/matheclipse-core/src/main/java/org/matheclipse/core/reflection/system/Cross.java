b57da57b0e974e80c0bdb64aef0078f46dbf956f
axexlck <axelclk@gmail.com>
2015-07-15 10:49:57 +0200

    Cross function

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Cross.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Cross.java
@@ -1,11 +1,9 @@
 package org.matheclipse.core.reflection.system;
 
-import static org.matheclipse.core.expression.F.List;
-import static org.matheclipse.core.expression.F.Plus;
-import static org.matheclipse.core.expression.F.Times;
+import static org.matheclipse.core.expression.F.*;
 
-import org.matheclipse.core.eval.interfaces.AbstractArg2;
-import org.matheclipse.core.expression.F;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 
@@ -14,20 +12,32 @@ import org.matheclipse.core.interfaces.IExpr;
  * 
  * See: <a href="http://en.wikipedia.org/wiki/Cross_product">Wikipedia:Cross product</a>
  */
-public class Cross extends AbstractArg2 {
+public class Cross extends AbstractFunctionEvaluator {
 
 	public Cross() {
 	}
 
-	@Override
-	public IExpr e2ObjArg(final IExpr o0, final IExpr o1) {
-		if (o0.isList() && o1.isList()) {
-			final IAST v0 = (IAST) o0;
-			final IAST v1 = (IAST) o1;
-			if ((v0.size() == 4) || (v1.size() == 4)) {
-				return List(Plus(Times(v0.arg2(), v1.arg3()), Times(F.CN1, v0.arg3(), v1.arg2())),
-						Plus(Times(v0.arg3(), v1.arg1()), Times(F.CN1, v0.arg1(), v1.arg3())),
-						Plus(Times(v0.arg1(), v1.arg2()), Times(F.CN1, v0.arg2(), v1.arg1())));
+	public IExpr evaluate(final IAST ast) {
+		Validate.checkRange(ast, 2, 3);
+		IExpr arg1 = ast.arg1();
+		if (ast.size() == 3) {
+			IExpr arg2 = ast.arg2();
+			int dim1 = arg1.isVector();
+			int dim2 = arg2.isVector();
+			if (dim1 == 3 && dim2 == 3) {
+				final IAST v1 = (IAST) arg1;
+				final IAST v2 = (IAST) arg2;
+				if ((v1.size() == 4) || (v2.size() == 4)) {
+					return List(Plus(Times(v1.arg2(), v2.arg3()), Times(CN1, v1.arg3(), v2.arg2())),
+							Plus(Times(v1.arg3(), v2.arg1()), Times(CN1, v1.arg1(), v2.arg3())),
+							Plus(Times(v1.arg1(), v2.arg2()), Times(CN1, v1.arg2(), v2.arg1())));
+				}
+			}
+		} else if (ast.size() == 2) {
+			int dim1 = arg1.isVector();
+			if (dim1 == 2) {
+				final IAST v1 = (IAST) arg1;
+				return List(Negate(v1.arg2()), v1.arg1());
 			}
 		}
 		return null;
