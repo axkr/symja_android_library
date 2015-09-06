d0ca9e0e20b3fa2fc1bb70a59223a6e70777ff6c
axexlck <axelclk@gmail.com>
2015-09-06 12:34:16 +0200

    Don't use android.Config in Integrate

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Integrate.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Integrate.java
@@ -19,6 +19,7 @@ import static org.matheclipse.core.expression.F.Times;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.matheclipse.core.basic.Config;
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.eval.exception.AbortException;
 import org.matheclipse.core.eval.exception.RecursionLimitExceeded;
@@ -32,8 +33,6 @@ import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
 import org.matheclipse.core.polynomials.PartialFractionIntegrateGenerator;
-
-import android.util.Config;
 
 import com.google.common.base.Predicate;
 
