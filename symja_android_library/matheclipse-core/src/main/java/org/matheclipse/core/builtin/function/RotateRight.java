38d1500675cd1f5e7e27630501a0cb6844b02f93
axexlck <axelclk@gmail.com>
2015-09-06 12:28:17 +0200

    Fixed bug in RotateRight, RotateLeft

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/RotateRight.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/function/RotateRight.java
@@ -17,27 +17,22 @@ public class RotateRight extends AbstractCoreFunctionEvaluator {
 		Validate.checkRange(ast, 2, 3);
 
 		IExpr arg1 = F.eval(ast.arg1());
-		try {
-			if (arg1.isAST()) {
-				final IAST result = F.ast(arg1.head());
-				if (ast.size() == 2) {
+		if (arg1.isAST()) {
+			final IAST result = F.ast(arg1.head());
+			if (ast.size() == 2) {
+				ASTRange range = ((IAST) arg1).args();
+				range.rotateRight(result, 1);
+				// Rotating.rotateRight((IAST) list.arg1(), result, 1, 1);
+				return result;
+			} else {
+				IExpr arg2 = F.eval(ast.arg2());
+				if (arg2.isInteger()) {
+					int n = Validate.checkIntType(arg2);
 					ASTRange range = ((IAST) arg1).args();
-					range.rotateRight(result, 1);
-					// Rotating.rotateRight((IAST) list.arg1(), result, 1, 1);
+					range.rotateRight(result, n);
 					return result;
-				} else {
-					IExpr arg2 = F.eval(ast.arg2());
-					if (arg2.isInteger()) {
-						int n = Validate.checkIntType(ast, 2, 1);
-						ASTRange range = ((IAST) arg1).args();
-						range.rotateRight(result, n);
-						// Rotating.rotateRight((IAST) list.arg1(), result, n, 1);
-						return result;
-					}
 				}
-
 			}
-		} catch (final ArithmeticException e) {
 
 		}
 		return null;
