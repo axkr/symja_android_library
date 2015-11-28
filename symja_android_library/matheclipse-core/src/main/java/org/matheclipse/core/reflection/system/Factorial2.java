d67d568664f807394013e23160b782dfeda49130
axexlck <axelclk@gmail.com>
2015-11-28 16:31:29 +0100

    Factorial2() for IInteger

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Factorial2.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Factorial2.java
@@ -1,10 +1,7 @@
 package org.matheclipse.core.reflection.system;
 
-import java.math.BigInteger;
-
 import org.matheclipse.core.eval.interfaces.AbstractTrigArg1;
 import org.matheclipse.core.expression.F;
-import org.matheclipse.core.expression.NumberUtil;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.IInteger;
 import org.matheclipse.core.interfaces.ISymbol;
@@ -16,31 +13,31 @@ public class Factorial2 extends AbstractTrigArg1 {
 	}
 
 	public static IInteger factorial2(final IInteger iArg) {
-		BigInteger result = BigInteger.ONE;
-		final BigInteger biggi = iArg.getBigNumerator();
-		BigInteger start;
-		if (biggi.compareTo(BigInteger.ZERO) == -1) {
-			result = BigInteger.valueOf(-1);
-			if (NumberUtil.isOdd(biggi)) {
-				start = BigInteger.valueOf(-3);
+		IInteger result = F.C1;
+		final IInteger biggi = iArg;
+		IInteger start;
+		if (biggi.compareTo(F.C0) == -1) {
+			result = F.CN1;
+			if (biggi.isOdd()) {
+				start = F.CN3;
 			} else {
-				start = BigInteger.valueOf(-2);
+				start = F.CN2;
 			}
-			for (BigInteger i = start; i.compareTo(biggi) >= 0; i = i.add(BigInteger.valueOf(-2))) {
+			for (IInteger i = start; i.compareTo(biggi) >= 0; i = i.add(F.CN2)) {
 				result = result.multiply(i);
 			}
 		} else {
-			if (NumberUtil.isOdd(biggi)) {
-				start = BigInteger.valueOf(3);
+			if (biggi.isOdd()) {
+				start = F.C3;
 			} else {
-				start = BigInteger.valueOf(2);
+				start = F.C2;
 			}
-			for (BigInteger i = start; i.compareTo(biggi) <= 0; i = i.add(BigInteger.valueOf(2))) {
+			for (IInteger i = start; i.compareTo(biggi) <= 0; i = i.add(F.C2)) {
 				result = result.multiply(i);
 			}
 		}
 
-		return F.integer(result);
+		return result;
 	}
 
 	@Override
