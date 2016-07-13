b7ae1bfea29a03bad875eeac567d31686dce245e
axexlck <axelclk@gmail.com>
2016-07-14 00:10:57 +0200

    Interval function

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Interval.java
@@ -0,0 +1,44 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.exception.WrongArgumentType;
+import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISignedNumber;
+import org.matheclipse.core.interfaces.ISymbol;
+
+/**
+ * <code>Interval[{min1, max1},......]</code>
+ */
+public class Interval extends AbstractEvaluator {
+
+	public Interval() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast, EvalEngine engine) {
+		Validate.checkRange(ast, 2);
+
+		if (ast.isInterval1()) {
+			IAST list = (IAST) ast.arg1();
+			try {
+				ISignedNumber min = (ISignedNumber) list.arg1();
+				ISignedNumber max = (ISignedNumber) list.arg2();
+				if (min.greaterThan(max).isTrue()) {
+					throw new WrongArgumentType(ast, ast.get(1), 1, "Min > Mac in interval");
+				}
+			} catch (ClassCastException cca) {
+				// do nothing
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void setUp(final ISymbol symbol) {
+	}
+
+}
