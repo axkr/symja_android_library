3ad4300e685ab9f224ac9da6fb02300a9470710f
axexlck <axelclk@gmail.com>
2016-08-28 13:12:14 +0200

    SolveExample

---
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/examples/SolveExample.java
@@ -0,0 +1,30 @@
+package org.matheclipse.core.examples;
+
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+public class SolveExample {
+
+	public static void main(String[] args) {
+		try {
+			ExprEvaluator util = new ExprEvaluator();
+			IExpr result = util.evaluate("Solve(2*x==5 + 4*x,x)");
+			// print: {{x->-5/2}}
+			System.out.println(result.toString());
+
+			result = util.evaluate("Roots(2*x==5+4*x, x)");
+			// print: x==-5/2
+			System.out.println(result.toString());
+		} catch (SyntaxError e) {
+			// catch Symja parser errors here
+			System.out.println(e.getMessage());
+		} catch (MathException me) {
+			// catch Symja math errors here
+			System.out.println(me.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
