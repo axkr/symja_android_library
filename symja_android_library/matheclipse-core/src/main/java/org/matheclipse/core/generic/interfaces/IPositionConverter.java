9157b3554afb5d5df24aeb4cf634e49bd17f120e
axexlck <axelclk@gmail.com>
2016-09-24 09:50:01 +0200

    clean up

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/interfaces/IPositionConverter.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/interfaces/IPositionConverter.java
@@ -4,13 +4,17 @@ public interface IPositionConverter<T> {
 	/**
 	 * Convert the integer position number >= 0 into an object
 	 *
+	 * @param position
+	 *            which should be converted to an object
+	 * @return
 	 */
 	T toObject(int position);
 
 	/**
 	 * Convert the object into an integer number >= 0
 	 *
-	 * @param position the object which should be converted
+	 * @param position
+	 *            the object which should be converted
 	 * @return -1 if the conversion is not possible
 	 */
 	int toInt(T position);
