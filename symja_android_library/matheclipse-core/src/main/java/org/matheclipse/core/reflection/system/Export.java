7954d5c4f9343ee921f419f29fbcf6e52bb3eeac
axexlck <axelclk@gmail.com>
2016-12-20 18:34:09 +0100

    Import() Export() functions implemented with commons-csv

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Export.java
@@ -0,0 +1,84 @@
+package org.matheclipse.core.reflection.system;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.exception.WrongNumberOfArguments;
+import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.IStringX;
+
+/**
+ * Import some data from file system.
+ *
+ */
+public class Export extends AbstractEvaluator {
+
+	public Export() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast, EvalEngine engine) {
+		Validate.checkRange(ast, 3, 4);
+
+		if (!(ast.arg1() instanceof IStringX)) {
+			throw new WrongNumberOfArguments(ast, 1, ast.size() - 1);
+		}
+		String format = "Table";
+		if (ast.size() == 4) {
+			if (!(ast.arg3() instanceof IStringX)) {
+				throw new WrongNumberOfArguments(ast, 3, ast.size() - 1);
+			}
+			format = ((IStringX) ast.arg3()).toString();
+		}
+		IStringX arg1 = (IStringX) ast.arg1();
+		IExpr arg2 = ast.arg2();
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(arg1.toString());
+			if (format.equals("Table")) {
+				int[] dims = arg2.isMatrix();
+				if (dims != null) {
+					for (int j = 0; j < dims[0]; j++) {
+						IAST rowList = (IAST) arg2.getAt(j + 1);
+						for (int i = 1; i <= dims[1]; i++) {
+							if (rowList.get(i).isSignedNumber()) {
+								writer.append(rowList.get(i).toString());
+							} else {
+								writer.append("\"");
+								writer.append(rowList.get(i).toString());
+								writer.append("\"");
+							}
+							if (i < dims[1]) {
+								writer.append(" ");
+							}
+						}
+						writer.append("\n");
+					}
+					return arg1;
+				} else {
+					if (arg2.isList()) {
+
+					}
+				}
+			}
+
+		} catch (IOException ioe) {
+			engine.printMessage("Export: file " + arg1.toString() + " not found!");
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		return F.NIL;
+	}
+
+}
---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Import.java
@@ -0,0 +1,80 @@
+package org.matheclipse.core.reflection.system;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.matheclipse.core.convert.AST2Expr;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.eval.exception.WrongNumberOfArguments;
+import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.IStringX;
+import org.matheclipse.parser.client.Parser;
+import org.matheclipse.parser.client.ast.ASTNode;
+
+/**
+ * Import some data from file system.
+ *
+ */
+public class Import extends AbstractEvaluator {
+
+	public Import() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast, EvalEngine engine) {
+		Validate.checkSize(ast, 3);
+
+		if (!(ast.arg1() instanceof IStringX)) {
+			throw new WrongNumberOfArguments(ast, 1, ast.size() - 1);
+		}
+		if (!(ast.arg2() instanceof IStringX)) {
+			throw new WrongNumberOfArguments(ast, 2, ast.size() - 1);
+		}
+		IStringX arg1 = (IStringX) ast.arg1();
+		IStringX arg2 = (IStringX) ast.arg2();
+		FileReader reader = null;
+		try {
+			reader = new FileReader(arg1.toString());
+			if (arg2.contentEquals("Table")) {
+				AST2Expr ast2Expr = AST2Expr.CONST;
+				if (engine.isRelaxedSyntax()) {
+					ast2Expr = AST2Expr.CONST_LC;
+				}
+				final Parser parser = new Parser(engine.isRelaxedSyntax(), true);
+
+				CSVFormat csvFormat = CSVFormat.RFC4180.withDelimiter(' ');
+				Iterable<CSVRecord> records = csvFormat.parse(reader);
+				IAST rowList = F.List();
+				for (CSVRecord record : records) {
+					IAST columnList = F.List();
+					for (String string : record) {
+						final ASTNode node = parser.parse(string);
+						IExpr temp = ast2Expr.convert(node, engine);
+						columnList.append(temp);
+					}
+					rowList.append(columnList);
+				}
+				return rowList;
+			}
+
+		} catch (IOException ioe) {
+			engine.printMessage("Import: file " + arg1.toString() + " not found!");
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+
+		return F.NIL;
+	}
+
+}
