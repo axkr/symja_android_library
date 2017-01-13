712c54f66d2e5a019b15fb8a4511ff9c9b8c00b7
axexlck <axelclk@gmail.com>
2017-01-13 17:34:25 +0100

    PolyLog() rules

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/PolyLog.java
@@ -0,0 +1,31 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.interfaces.AbstractArg2;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+import org.matheclipse.core.reflection.system.rules.PolyLogRules;
+
+public class PolyLog extends AbstractArg2 implements PolyLogRules {
+	public PolyLog() {
+		// default ctor
+	}
+
+	@Override
+	public IExpr e2ObjArg(IExpr o0, IExpr o1) {
+		return F.NIL;
+	}
+
+	@Override
+	public IAST getRuleAST() {
+		return RULES;
+	}
+
+	@Override
+	public void setUp(final ISymbol newSymbol) {
+		newSymbol.setAttributes(ISymbol.LISTABLE | ISymbol.NUMERICFUNCTION);
+		super.setUp(newSymbol);
+	}
+
+}
