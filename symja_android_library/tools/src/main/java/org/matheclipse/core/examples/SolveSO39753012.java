a491bbf509c46f27c69b357d94b8c09c2ccd0c28
axexlck <axelclk@gmail.com>
2017-03-27 20:28:34 +0200

    Stackoverflow examples

---
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/examples/SolveSO39753012.java
@@ -0,0 +1,47 @@
+package org.matheclipse.core.examples;
+
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+/**
+ * See <a href="http://stackoverflow.com/questions/39753012">Stackoverflow 39753012</a>
+ */
+public class SolveSO39753012 {
+
+	public static void main(String[] args) {
+		try {
+			ExprEvaluator util = new ExprEvaluator();
+			IExpr eq = F.Equal(F.Plus(F.a, F.b), F.c);
+			IExpr eq1 = eq.replaceAll(F.Rule(F.a, F.integer(1)));
+			eq1 = eq1.replaceAll(F.Rule(F.b, F.integer(2)));
+
+			// Solve(1+2==c, c)
+			IExpr result = util.evaluate(F.Solve(eq1, F.c));
+			// print: {{c->3}}
+			System.out.println(result.toString());
+
+			IExpr eq2 = eq.replaceAll(F.Rule(F.a, F.integer(1)));
+			eq2 = eq2.replaceAll(F.Rule(F.c, F.integer(3)));
+			// Solve(1+b==3, b)
+			result = util.evaluate(F.Solve(eq2, F.b));
+			// print: {{b->2}}
+			System.out.println(result.toString());
+
+		} catch (SyntaxError e) {
+			// catch Symja parser errors here
+			System.out.println(e.getMessage());
+		} catch (MathException me) {
+			// catch Symja math errors here
+			System.out.println(me.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} catch (final StackOverflowError soe) {
+			System.out.println(soe.getMessage());
+		} catch (final OutOfMemoryError oome) {
+			System.out.println(oome.getMessage());
+		}
+	}
+}
---
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/examples/SolveSO43024172.java
@@ -0,0 +1,68 @@
+package org.matheclipse.core.examples;
+
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+/**
+ * See <a href="http://stackoverflow.com/questions/43024172">Stackoverflow 43024172</a>
+ */
+public class SolveSO43024172 {
+
+	public static void main(String[] args) {
+		try {
+			ExprEvaluator util = new ExprEvaluator();
+			IExpr expr = util.evaluate("(x1)^2+4*(x2)^2-2*x1-4*x2");
+			System.out.println(expr.toString());
+
+			IExpr a = util.evaluate("a");
+			IExpr x1 = util.evaluate("x1");
+			IExpr x2 = util.evaluate("x2");
+			IExpr x1Substitute = util.evaluate("5+a");
+			IExpr x2Substitute = util.evaluate("2");
+
+			IAST astRules = F.List(F.Rule(x1, x1Substitute), F.Rule(x2, x2Substitute));
+			// {x1->5+a,x2->2}
+			System.out.println(astRules.toString());
+
+			IExpr replacedExpr = expr.replaceAll(astRules);
+			// -2*(5+a)+(5+a)^2+(-4)*2+4*2^2
+			System.out.println(replacedExpr.toString());
+			// 8+(5+a)^2-2*(5+a)
+			replacedExpr = util.evaluate(replacedExpr);
+			System.out.println(replacedExpr.toString());
+
+			// replacedExpr = util.evaluate(F.ExpandAll(replacedExpr));
+			// 23+8*a+a^2
+			// System.out.println(replacedExpr.toString());
+
+			IExpr derivedExpr = util.evaluate(F.D(replacedExpr, a));
+			// -2+2*(5+a)
+			System.out.println(derivedExpr.toString());
+
+			IExpr equation = F.Equal(derivedExpr, F.C0);
+			// -2+2*(5+a)==0
+			System.out.println(equation.toString());
+
+			IExpr solvedEquation = util.evaluate(F.Solve(equation, a));
+			// {{a->-4}}
+			System.out.println(solvedEquation.toString());
+
+		} catch (SyntaxError e) {
+			// catch Symja parser errors here
+			System.out.println(e.getMessage());
+		} catch (MathException me) {
+			// catch Symja math errors here
+			System.out.println(me.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} catch (final StackOverflowError soe) {
+			System.out.println(soe.getMessage());
+		} catch (final OutOfMemoryError oome) {
+			System.out.println(oome.getMessage());
+		}
+	}
+}
