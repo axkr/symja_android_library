cb63df446b89fd187aa47bd6e77bad8301047125
axexlck <axelclk@gmail.com>
2018-02-09 16:10:42 +0100

    Console tests

--- symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/ConsoleTestCase.java
+++ symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/ConsoleTestCase.java
@@ -14,33 +14,31 @@ public class ConsoleTestCase extends AbstractTestCase {
 	}
 
 	public void test001() {
-		String[] args = new String[] { "-c", "D(sin(x)^3,x)" }; 
-		 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos); 
-		PrintStream old = System.out; 
-		System.setOut(ps); 
-		
-		Console.main(args); 
-		
-		System.out.flush();
-		System.setOut(old); 
-		assertEquals(baos.toString(), //
-				"3*Cos(x)*Sin(x)^2");
+		String[] args = new String[] { "-c", "D(sin(x)^3,x)" };
+		check(args, "3*Cos(x)*Sin(x)^2");
 	}
 
 	public void test002() {
 		String[] args = new String[] { "-f", "Factorial", "-a", "20" };
- 
+		check(args, "2432902008176640000");
+	}
+
+	public void test003() {
+		String[] args = new String[] { "-c", "f(x_,y_):={x,y}; f(a,b)" };
+		check(args, "{a,b}");
+	}
+
+	private void check(String[] args, String result) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		PrintStream ps = new PrintStream(baos); 
-		PrintStream old = System.out; 
-		System.setOut(ps); 
-		Console.main(args); 
-		
+		PrintStream ps = new PrintStream(baos);
+		PrintStream old = System.out;
+		System.setOut(ps);
+
+		Console.main(args);
+
 		System.out.flush();
-		System.setOut(old); 
+		System.setOut(old);
 		assertEquals(baos.toString(), //
-				"2432902008176640000");
+				result);
 	}
 }
