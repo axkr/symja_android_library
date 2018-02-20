556ee43f9834d5f8f1cb491dd86a316616dfc05d
Duy <tranleduy1233@gmail.com>
2018-02-20 13:17:51 +0700

    remove default method IASTMutable. All test passed

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/interfaces/IASTAppendableImpl.java
+++
@@ -1,8 +0,0 @@
-package org.matheclipse.core.interfaces;
-
-/**
- * Created by Duy on 2/20/2018.
- */
-
-public abstract class IASTAppendableImpl extends IASTMutableImpl implements IASTAppendable {
-}
