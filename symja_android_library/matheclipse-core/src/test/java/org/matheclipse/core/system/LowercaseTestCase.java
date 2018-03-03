6c82cd2c006ac436bb9e3f0c612bdcce0c028a14
axexlck <axelclk@gmail.com>
2018-03-03 05:25:57 +0100

    markdown MangoldtLambda

--- symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
+++ symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
@@ -3910,7 +3910,7 @@ public class LowercaseTestCase extends AbstractTestCase {
 
 	public void testMangoldtLambda() {
 		check("MangoldtLambda(3^5)", "Log(3)");
-		check("MangoldtLambda({1,2,6})", "{0,Log(2),0}");
+		check("MangoldtLambda({1,2,3,4,5,6,7,8,9})", "{0,Log(2),Log(3),Log(2),Log(5),0,Log(7),Log(2),Log(3)}");
 		check("{MangoldtLambda(Prime(10^5)^10), MangoldtLambda(2*Prime(10^5)^10)}", "{Log(1299709),0}");
 	}
 
