e4bb6b7f59a987b420d59a68eada21a3ed1316bd
axexlck <axelclk@gmail.com>
2018-06-16 18:00:57 +0200

    #55 Use java10 jshell to run a java/symja repl

--- symja_android_library/symja-jshell.bat
+++ symja_android_library/symja-jshell.bat
@@ -1,6 +1,6 @@
 REM change directory to symja installation folder
-REM cd /symja
-REM run symja Console
+REM cd /temp/symja
+REM run start-symja.bat
 SET "JAVA_HOME=C:\Program Files\Java\jdk-10.0.1"
 
 "%JAVA_HOME%\bin\jshell" --class-path "/temp/symja/lib/*" --startup start-symja.jsh
