3a81f3af4e78937df1d1712f0937ea6bac0f3541
axexlck <axelclk@gmail.com>
2018-07-29 11:30:00 +0200

    Missing Callable files

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/EvalCallable.java
@@ -0,0 +1,47 @@
+package org.matheclipse.core.eval;
+
+import java.util.concurrent.Callable;
+
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IExpr;
+
+class EvalCallable implements Callable<IExpr> {
+	private final EvalEngine fEngine;
+	private IExpr fExpr;
+
+	public EvalCallable(EvalEngine engine) {
+		this(F.Null, engine);
+	}
+
+	public EvalCallable(IExpr expr, EvalEngine engine) {
+		fExpr = expr;
+		fEngine = engine;
+	}
+
+	/**
+	 * Basic call which catches no exceptions
+	 */
+	@Override
+	public IExpr call() throws Exception {
+		EvalEngine.set(fEngine);
+		// try {
+		fEngine.reset();
+		IExpr temp = fEngine.evaluate(fExpr);
+		if (!fEngine.isOutListDisabled()) {
+			fEngine.addOut(temp);
+		}
+		return temp;
+		// } finally {
+		// EvalEngine.remove();
+		// }
+	}
+
+	public IExpr getExpr() {
+		return fExpr;
+	}
+
+	public void setExpr(IExpr fExpr) {
+		this.fExpr = fExpr;
+	}
+
+}
---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/EvalControlledCallable.java
@@ -0,0 +1,52 @@
+package org.matheclipse.core.eval;
+
+import java.io.StringWriter;
+import java.util.concurrent.Callable;
+
+import org.matheclipse.core.eval.exception.Validate;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+class EvalControlledCallable extends EvalCallable {
+	public EvalControlledCallable(EvalEngine engine) {
+		super( engine);
+	}
+
+	@Override
+	public IExpr call() throws Exception {
+		final StringWriter buf = new StringWriter();
+		try {
+			return super.call();
+		} catch (final SyntaxError se) {
+			String msg = se.getMessage();
+			System.err.println(msg);
+			System.err.println();
+			System.err.flush();
+		} catch (final RuntimeException re) {
+			Throwable me = re.getCause();
+			if (me instanceof MathException) {
+				Validate.printException(buf, me);
+			} else {
+				Validate.printException(buf, re);
+			}
+			System.err.println(buf.toString());
+			System.err.flush();
+		} catch (final Exception e) {
+			Validate.printException(buf, e);
+			System.err.println(buf.toString());
+			System.err.flush();
+		} catch (final OutOfMemoryError e) {
+			Validate.printException(buf, e);
+			System.err.println(buf.toString());
+			System.err.flush();
+		} catch (final StackOverflowError e) {
+			Validate.printException(buf, e);
+			System.err.println(buf.toString());
+			System.err.flush();
+		}
+		return F.$Aborted;
+	}
+
+}
