b846bb8df5cacc78b29d8fd27f276039eb065e85
axexlck <axelclk@gmail.com>
2018-10-01 23:48:02 +0200

    Missing TimeoutException file

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/exception/TimeoutException.java
@@ -0,0 +1,13 @@
+package org.matheclipse.core.eval.exception;
+
+public class TimeoutException extends FlowControlException {
+
+	private static final long serialVersionUID = -1664843691177511531L;
+	
+	public final static TimeoutException TIMED_OUT = new TimeoutException();
+	
+	public TimeoutException() {
+		super("Timed out evaluation.");
+	}
+
+}
