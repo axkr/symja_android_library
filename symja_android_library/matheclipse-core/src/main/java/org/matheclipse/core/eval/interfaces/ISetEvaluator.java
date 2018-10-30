3386ce7f7a86c4ae4e0520f134f937441c6b8856
axexlck <axelclk@gmail.com>
2018-10-30 21:42:53 +0100

    #99 VectorAnalysis package
    - add missing interface

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/interfaces/ISetEvaluator.java
@@ -0,0 +1,20 @@
+package org.matheclipse.core.eval.interfaces;
+
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+
+/**
+ * <p>
+ * Interface for &quot;core functions&quot; which can specially evaluated like <code>Attributes, List, Part</code>
+ * </p>
+ * 
+ */
+public interface ISetEvaluator extends IFunctionEvaluator {
+	
+	default IExpr evaluateSet(final IExpr leftHandSide, IExpr rightHandSide, EvalEngine engine) {
+		return F.NIL;
+	}
+}
