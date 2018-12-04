2a488311f75d26fda4c877dbcd8d8712f0ad7554
axexlck <axelclk@gmail.com>
2018-12-04 19:44:16 +0100

    FunctionExpand for Binomial

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/FunctionExpand.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/FunctionExpand.java
@@ -1,6 +1,7 @@
 package org.matheclipse.core.reflection.system;
 
 import static org.matheclipse.core.expression.F.Beta;
+import static org.matheclipse.core.expression.F.Binomial;
 import static org.matheclipse.core.expression.F.ChebyshevT;
 import static org.matheclipse.core.expression.F.ChebyshevU;
 import static org.matheclipse.core.expression.F.Factorial;
@@ -8,6 +9,7 @@ import static org.matheclipse.core.expression.F.a;
 import static org.matheclipse.core.expression.F.a_;
 import static org.matheclipse.core.expression.F.b;
 import static org.matheclipse.core.expression.F.b_;
+import static org.matheclipse.core.expression.F.c;
 import static org.matheclipse.core.expression.F.j;
 import static org.matheclipse.core.expression.F.k;
 import static org.matheclipse.core.expression.F.m;
@@ -70,6 +72,22 @@ public class FunctionExpand extends AbstractEvaluator {
 						F.Times(F.Factorial(F.Plus(F.CN1, a)),
 								F.Product(F.Power(F.Plus(k, b), -1), F.List(k, F.C0, F.Plus(F.CN1, a)))),
 						F.And(F.IntegerQ(a), F.Greater(a, F.C0)))); // $$);
+
+		MATCHER.caseOf(Binomial(a_, b_), //
+				// [$ If(IntegerQ(b)&&Positive(b),Product(a-c,{c,0,b-1})/b!, If(IntegerQ(a)&&Positive(a),
+				// (a!*Sin(b*Pi))/(Product(b-c,{c,0,a})*Pi), Gamma(1 + a)/(Gamma(1 + b)*Gamma(1 - b + a))) ) $]
+				F.If(F.And(F.IntegerQ(b), F.Positive(b)),
+						F.Times(F.Power(F.Factorial(b), -1), F.Product(F.Plus(a, F.Negate(c)),
+								F.List(c, F.C0, F.Plus(F.CN1, b)))),
+						F.If(F.And(F.IntegerQ(a), F
+								.Positive(a)), F
+										.Times(F.Power(
+												F.Times(F.Product(F.Plus(b, F.Negate(c)), F.List(c, F.C0, a)),
+														F.Pi),
+												-1), F.Factorial(a), F.Sin(F.Times(b, F.Pi))),
+								F.Times(F.Gamma(F.Plus(F.C1, a)), F.Power(
+										F.Times(F.Gamma(F.Plus(F.C1, b)), F.Gamma(F.Plus(F.C1, F.Negate(b), a))),
+										-1))))); // $$);
 
 		// CatalanNumber
 		MATCHER.caseOf(F.CatalanNumber(n_), //
