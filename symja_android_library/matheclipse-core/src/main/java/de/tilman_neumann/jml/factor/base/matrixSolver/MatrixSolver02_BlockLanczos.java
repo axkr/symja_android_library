c665401751043c70a0a33b2c6a598c71d9572d65
axexlck <axelclk@gmail.com>
2019-01-07 20:12:32 +0100

    delete org.junit imports

--- symja_android_library/matheclipse-core/src/main/java/de/tilman_neumann/jml/factor/base/matrixSolver/MatrixSolver02_BlockLanczos.java
+++ symja_android_library/matheclipse-core/src/main/java/de/tilman_neumann/jml/factor/base/matrixSolver/MatrixSolver02_BlockLanczos.java
@@ -13,8 +13,6 @@
  */
 package de.tilman_neumann.jml.factor.base.matrixSolver;
 
-import static org.junit.Assert.assertTrue;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -34,61 +32,63 @@ public class MatrixSolver02_BlockLanczos extends MatrixSolver {
 	private static final boolean DEBUG = false;
 
 	private BlockLanczos blockLanczosSolver = new BlockLanczos();
-	
+
 	@Override
 	public String getName() {
 		return "solver02_BL";
 	}
-	
+
 	@Override
-	protected void solve(List<Congruence> congruences, Map<Integer, Integer> factors_2_columnIndices) throws FactorException {
+	protected void solve(List<Congruence> congruences, Map<Integer, Integer> factors_2_columnIndices)
+			throws FactorException {
 		// create the matrix:
 		// * the rows are in the same order as in the congruences list
 		// * we fill a row with the column indices where the congruence has a factor with odd exponent
 		int matrixBlength = congruences.size();
 		int[][] matrixB = new int[matrixBlength][];
-		int i=0;
+		int i = 0;
 		for (Congruence congruence : congruences) {
 			// row entry = set of column indices where the congruence has a factor with odd exponent
 			Integer[] oddExpFactors = congruence.getMatrixElements();
 			int[] matrixRow = new int[oddExpFactors.length];
-			int j=0;
+			int j = 0;
 			for (Integer oddExpFactor : oddExpFactors) {
 				// columnIndex should not be bigger than the number of congruences
 				int columnIndex = factors_2_columnIndices.get(oddExpFactor);
-				if (DEBUG) assertTrue(columnIndex <= matrixBlength);
+				// if (DEBUG) assertTrue(columnIndex <= matrixBlength);
 				matrixRow[j++] = columnIndex;
 			}
 			matrixB[i++] = matrixRow;
 		}
-		//LOG.debug("constructed matrix with " + matrixBlength + " rows and " + factors_2_columnIndices.size() + " columns");
-		
+		// LOG.debug("constructed matrix with " + matrixBlength + " rows and " + factors_2_columnIndices.size() + "
+		// columns");
+
 		// invoke Alperns Block Lanczos solver
 		int[] matrixV = blockLanczosSolver.computeBlockLanczos(matrixB, matrixBlength);
-		//LOG.debug("BlockLanzcos returned matrixV = " + Arrays.toString(matrixV));
-		
+		// LOG.debug("BlockLanzcos returned matrixV = " + Arrays.toString(matrixV));
+
 		// See Siqs.LinearAlgebraPhase() for how to interprete matrixV:
 		// There can be up to 32 potential solutions encoded in matrixV!
 		// One in bit 0 of all ints, the next in bit 1 of all ints, and so on.
-  		for (int mask = 1; mask != 0; mask *= 2) {
-  			HashSet<AQPair> totalAQPairs = new HashSet<AQPair>(); // Set required for the "xor"-operation below
-  			for (int row = matrixBlength - 1; row >= 0; row--) {
-  				if ((matrixV[row] & mask) != 0) {
-  					// the current row belongs to the solution encoded in matrixV by the bit addressed by mask.
-  					// the row indices are the same as in my congruences list.
-  					Congruence congruence = congruences.get(row);
-  					//LOG.info("mask=" + mask + ": add congruence " + congruence);
+		for (int mask = 1; mask != 0; mask *= 2) {
+			HashSet<AQPair> totalAQPairs = new HashSet<AQPair>(); // Set required for the "xor"-operation below
+			for (int row = matrixBlength - 1; row >= 0; row--) {
+				if ((matrixV[row] & mask) != 0) {
+					// the current row belongs to the solution encoded in matrixV by the bit addressed by mask.
+					// the row indices are the same as in my congruences list.
+					Congruence congruence = congruences.get(row);
+					// LOG.info("mask=" + mask + ": add congruence " + congruence);
 					// add the new AQ-pairs via "xor"
 					congruence.addMyAQPairsViaXor(totalAQPairs);
-  				}
-  			}
-  			
-  			if (!totalAQPairs.isEmpty()) {
-  				// Sometimes the BlockLanczos() method returns non-null-vectors (having q-factors with odd exponent),
-  				// but it did not seem beneficial to test the exponents before calling processNullVector(.)
-	  			// So just "return" the AQ-pairs of the null vector:
-	  			nullVectorProcessor.processNullVector(totalAQPairs);
-  			}
-  		}
+				}
+			}
+
+			if (!totalAQPairs.isEmpty()) {
+				// Sometimes the BlockLanczos() method returns non-null-vectors (having q-factors with odd exponent),
+				// but it did not seem beneficial to test the exponents before calling processNullVector(.)
+				// So just "return" the AQ-pairs of the null vector:
+				nullVectorProcessor.processNullVector(totalAQPairs);
+			}
+		}
 	}
 }
