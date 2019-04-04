cd168d5228ae4b5a23918b64253f8c64798662b1
axexlck <axelclk@gmail.com>
2019-04-04 20:01:15 +0200

    #121 number of variables not set in Algebra#complexFactor()
    - factor(x^(12)-y^(12),GaussianIntegers->True) is still very very slow
      see: https://github.com/kredel/java-algebra-system/issues/10

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/Algebra.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/builtin/Algebra.java
@@ -2064,14 +2064,15 @@ public class Algebra {
 			if (option.isReal()) {
 				return factorModulus(expr, varList, factorSquareFree, option);
 			}
-
-			option = options.getOption("GaussianIntegers");
-			if (option.isTrue()) {
-				return factorComplex(expr, varList, F.Times, false, false, engine);
-			}
-			option = options.getOption("Extension");
-			if (option.isImaginaryUnit()) {
-				return factorComplex(expr, varList, F.Times, false, false, engine);
+			if (!factorSquareFree) {
+				option = options.getOption("GaussianIntegers");
+				if (option.isTrue()) {
+					return factorComplex(expr, varList, F.Times, false, false, engine);
+				}
+				option = options.getOption("Extension");
+				if (option.isImaginaryUnit()) {
+					return factorComplex(expr, varList, F.Times, false, false, engine);
+				}
 			}
 			return F.NIL; // no evaluation
 		}
@@ -5039,11 +5040,7 @@ public class Algebra {
 	 * @throws JASConversionException
 	 */
 	public static IAST factorComplex(IExpr expr, List<IExpr> varList, ISymbol head, boolean noGCDLCM,
-			boolean numeric2Rational, EvalEngine engine)  {
-		if (varList.size() > 1) {
-			engine.printMessage("Factor: GaussianIntegers for multivariate polynomials are not supported.");
-			return F.NIL;
-		}
+			boolean numeric2Rational, EvalEngine engine) {
 		try {
 			JASConvert<BigRational> jas = new JASConvert<BigRational>(varList, BigRational.ZERO);
 			GenPolynomial<BigRational> polyRat = jas.expr2JAS(expr, numeric2Rational);
@@ -5070,7 +5067,8 @@ public class Algebra {
 		GenPolynomial<BigRational> poly = (GenPolynomial<BigRational>) objects[2];
 
 		ComplexRing<BigRational> cfac = new ComplexRing<BigRational>(BigRational.ZERO);
-		GenPolynomialRing<Complex<BigRational>> cpfac = new GenPolynomialRing<Complex<BigRational>>(cfac, 1, termOrder);
+		GenPolynomialRing<Complex<BigRational>> cpfac = new GenPolynomialRing<Complex<BigRational>>(cfac,
+				varList.size(), termOrder);
 		GenPolynomial<Complex<BigRational>> a = PolyUtil.complexFromAny(cpfac, poly);
 		FactorComplex<BigRational> factorAbstract = new FactorComplex<BigRational>(cfac);
 		SortedMap<GenPolynomial<Complex<BigRational>>, Long> map = factorAbstract.factors(a);
