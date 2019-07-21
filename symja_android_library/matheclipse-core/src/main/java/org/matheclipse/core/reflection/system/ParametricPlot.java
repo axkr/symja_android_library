fd50174604a89fb36f62c1625dc2737dbe7476e3
axexlck <axelclk@gmail.com>
2019-07-21 17:19:51 +0200

    ParametricPlot

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/ParametricPlot.java
@@ -0,0 +1,38 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.basic.Config;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+
+/**
+ * Plots x/y functions
+ *
+ */
+public class ParametricPlot extends AbstractEvaluator {
+	/**
+	 * Constructor for the singleton
+	 */
+	public final static ParametricPlot CONST = new ParametricPlot();
+
+	// private final static int N = 100;
+
+	public ParametricPlot() {
+	}
+
+	@Override
+	public IExpr evaluate(final IAST ast, EvalEngine engine) {
+		if (Config.USE_MATHCELL) {
+			return F.Manipulate.of(engine, ast);
+		}
+		return F.NIL;
+	}
+
+	@Override
+	public void setUp(final ISymbol newSymbol) {
+		newSymbol.setAttributes(ISymbol.HOLDALL);
+	}
+}
