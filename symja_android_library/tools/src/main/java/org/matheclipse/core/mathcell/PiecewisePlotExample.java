d4618dd97cba7314433f8ceb1c486e9b3b49d575
axexlck <axelclk@gmail.com>
2019-08-15 18:09:11 +0200

    PiecewisePlotExample

---
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/PiecewisePlotExample.java
@@ -0,0 +1,76 @@
+package org.matheclipse.core.mathcell;
+
+import org.matheclipse.core.basic.Config;
+import org.matheclipse.core.eval.ExprEvaluator;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.parser.client.SyntaxError;
+import org.matheclipse.parser.client.math.MathException;
+
+public class PiecewisePlotExample {
+	private final static String WEB_PAGE = //
+			"<html>\n" + //
+					"<head>\n" + //
+					"<meta charset=\"utf-8\">\n" + //
+					"<title>MathCell</title>\n" + //
+					"</head>\n" + //
+					"\n" + //
+					"<body>\n" + //
+					"\n" + //
+					"<script src=\"https://cdn.jsdelivr.net/gh/paulmasson/math@1.2.2/build/math.js\"></script>" + //
+					"\n" + //
+					"<script src=\"https://cdn.jsdelivr.net/gh/paulmasson/mathcell@1.7.0/build/mathcell.js\"></script>\n"
+					+ //
+						// "<script
+						// src=\"https://cdn.jsdelivr.net/gh/mathjax/MathJax@2.7.5/MathJax.js?config=TeX-AMS_HTML\"></script>"
+						// + //
+					"\n" + //
+					"<p style=\"text-align: center; line-height: 2\"><span style=\"font-size: 20pt\">MathCell</span></p>\n"
+					+ //
+					"\n" + //
+					"<div class=\"mathcell\" style=\"height: 4in\">\n" + //
+					"<script>\n" + //
+					"\n" + //
+					"var parent = document.scripts[ document.scripts.length - 1 ].parentNode;\n" + //
+					"\n" + //
+					"var id = generateId();\n" + //
+					"parent.id = id;\n" + //
+					"\n" + //
+					"`1`\n" + //
+					"\n" + //
+					"parent.update( id );\n" + //
+					"\n" + //
+					"</script>\n" + //
+					"</div>\n" + //
+					"\n" + //
+					"</body>\n" + //
+					"</html>";//
+
+	public static void main(String[] args) {
+		try {
+			Config.USE_MATHCELL = true;
+			ExprEvaluator util = new ExprEvaluator();
+
+			IExpr result = util.eval("Plot(Piecewise({{x^2, x < 0}, {x, x >= 0&&x<1},{Cos(x-1), x >= 1}}), {x, -2, 12})");
+			if (result.isAST(F.JSFormData, 3) && result.second().toString().equals("mathcell")) {
+				String manipulateStr = ((IAST) result).arg1().toString();
+				String js = WEB_PAGE;
+				js = js.replaceAll("`1`", manipulateStr);
+				System.out.println(js);
+			}
+		} catch (SyntaxError e) {
+			// catch Symja parser errors here
+			System.out.println(e.getMessage());
+		} catch (MathException me) {
+			// catch Symja math errors here
+			System.out.println(me.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} catch (final StackOverflowError soe) {
+			System.out.println(soe.getMessage());
+		} catch (final OutOfMemoryError oome) {
+			System.out.println(oome.getMessage());
+		}
+	}
+}
