c6c292346d7d2e5520e38711d290861a072d77fb
axexlck <axelclk@gmail.com>
2019-08-21 03:29:27 +0200

    New GrapExpr class

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/expression/data/GraphExpr.java
@@ -0,0 +1,41 @@
+package org.matheclipse.core.expression.data;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
+import org.matheclipse.core.expression.DataExpr;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IExpr;
+
+public class GraphExpr<T> extends DataExpr<Graph<IExpr, T>> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6160043985328230156L;
+
+	/**
+	 * Be cautious with this method, no new internal IExpr object is created
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static <T> GraphExpr<T> newInstance(final Graph<IExpr, T> value) {
+		return new GraphExpr<T>(value);
+	}
+
+	protected GraphExpr(final Graph<IExpr, T> graph) {
+		super(F.Graph, graph);
+	}
+
+	/**
+	 * Test if the graph is instance of <code>DefaultDirectedWeightedGraph</code> or
+	 * <code>DefaultUndirectedWeightedGraph</code>
+	 * 
+	 * @param graph
+	 *            the graph which should be tested.
+	 * @return <code>true</code> if the graph is a weighted graph
+	 */
+	public   boolean isWeightedGraph( ) {
+		return fData instanceof DefaultDirectedWeightedGraph || fData instanceof DefaultUndirectedWeightedGraph;
+	}
+}
