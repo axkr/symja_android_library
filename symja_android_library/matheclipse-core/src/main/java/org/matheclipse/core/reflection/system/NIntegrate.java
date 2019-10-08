721f918777c27179664bf1d21d09584d73c77f19
axexlck <axelclk@gmail.com>
2019-10-08 10:34:57 +0200

    #150 avoid StackOverflow

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/NIntegrate.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/NIntegrate.java
@@ -7,13 +7,13 @@ import org.hipparchus.analysis.integration.TrapezoidIntegrator;
 import org.hipparchus.analysis.integration.UnivariateIntegrator;
 import org.hipparchus.analysis.integration.gauss.GaussIntegrator;
 import org.hipparchus.analysis.integration.gauss.GaussIntegratorFactory;
+import org.hipparchus.exception.LocalizedCoreFormats;
+import org.hipparchus.exception.MathIllegalArgumentException;
 import org.hipparchus.exception.MathIllegalStateException;
 import org.hipparchus.util.Precision;
 import org.matheclipse.core.basic.Config;
 import org.matheclipse.core.builtin.IOFunctions;
 import org.matheclipse.core.eval.EvalEngine;
-import org.matheclipse.core.eval.exception.Validate;
-import org.matheclipse.core.eval.exception.WrappedException;
 import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
 import org.matheclipse.core.eval.util.OptionArgs;
 import org.matheclipse.core.expression.F;
@@ -113,6 +113,11 @@ public class NIntegrate extends AbstractFunctionEvaluator {
 		} else if ("Trapezoid".equalsIgnoreCase(method)) {
 			integrator = new TrapezoidIntegrator();
 		} else {
+			if (maxPoints > 1000) {
+				// github 150 - avoid StackOverflow from recursion
+				// see also https://github.com/Hipparchus-Math/hipparchus/issues/61
+				throw new MathIllegalArgumentException(LocalizedCoreFormats.NUMBER_TOO_LARGE, maxPoints, 1000);
+			}
 			// default: LegendreGauss
 			GaussIntegrator integ = factory.legendre(maxPoints, min, max);
 			return integ.integrate(f);
