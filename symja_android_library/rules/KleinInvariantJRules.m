41c5b6ce276857699f361c1ba3e0d4f7fb1c6ee6
axexlck <axelclk@gmail.com>
2020-03-14 09:50:40 +0100

    KleinInvariantJ rules

---
+++ symja_android_library/rules/KleinInvariantJRules.m
@@ -0,0 +1,18 @@
+{   
+  KleinInvariantJ( (-1+I*Sqrt[3]) / 2 ) = 1,
+  KleinInvariantJ( (1+I*Sqrt[3]) / 2 ) = 0,
+  KleinInvariantJ( (1+I*Sqrt[7]) / 2 ) = -125/64,
+  KleinInvariantJ( (1+I*Sqrt[11]) / 2 ) = -512/27,
+  KleinInvariantJ( (1+I*Sqrt[19]) / 2 ) = -512,
+  KleinInvariantJ( (1+I*3*Sqrt[3]) / 2 ) = -64000/9,
+  KleinInvariantJ( (1+I*Sqrt[43]) / 2 ) = -512000,
+  KleinInvariantJ( (1+I*Sqrt[67]) / 2 ) = -85184000,
+  KleinInvariantJ( (1+I*Sqrt[163]) / 2 ) = -151931373056000,
+  
+  KleinInvariantJ(E^(Pi*(I/3))) = 0,
+  KleinInvariantJ(E^(2*Pi*(I/3))) = 0,
+  KleinInvariantJ(1/2*I*Sqrt(3)+1/2) = 0,
+  KleinInvariantJ(1/2*I*Sqrt(3)-1/2) = 0,
+  
+  KleinInvariantJ(I*Infinity) = Infinity
+}
