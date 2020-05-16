7cbe11949313c92e87048b1b8d289b48a5ade00b
axexlck <axelclk@gmail.com>
2020-05-16 18:47:59 +0200

    WIP: Restructuring project into submodules / update libs
    - snuggletex
    
    new:
    - matheclipse-api
    - matheclipse-frontend

--- symja_android_library/matheclipse-core/src/main/java/uk/ac/ed/ph/snuggletex/definitions/MathVariantMap.java
+++ symja_android_library/matheclipse-core/src/main/java/uk/ac/ed/ph/snuggletex/definitions/MathVariantMap.java
@@ -6,49 +6,50 @@
 package uk.ac.ed.ph.snuggletex.definitions;
 
 /**
- * This map encapsulates how to apply certain types of Mathematical character variants to
- * "safe and plain" Unicode characters by mapping them to other characters.
+ * This map encapsulates how to apply certain types of Mathematical character variants to "safe and plain" Unicode
+ * characters by mapping them to other characters.
  * 
  * @see MathVariantMaps
  *
- * @author  David McKain
+ * @author David McKain
  * @version $Revision: 525 $
  */
 public final class MathVariantMap {
-    
-    private static final int MIN_CODEPOINT = 32;
-    private static final int MAX_CODEPOINT = 128;
 
-    private final String mathVariantName;
-    private final char[] accentByCodePoint;
-    
-    public MathVariantMap(final String mathVariantName, final char[] accentData) {
-        this.mathVariantName = mathVariantName;
-        this.accentByCodePoint = new char[MAX_CODEPOINT - MIN_CODEPOINT];
-        char ascii, accented;
-        int index;
-        for (int i=0; i<accentData.length; ) {
-            ascii = accentData[i++];
-            accented = accentData[i++];
-            index = charToIndex(ascii);
-            if (index!=-1) {
-                accentByCodePoint[index] = accented;
-            }
-        }
-    }
-    
-    public String getMathVariantName() {
-        return mathVariantName;
-    }
+	private static final int MIN_CODEPOINT = 32;
+	private static final int MAX_CODEPOINT = 128;
 
-    public char getAccentedChar(char c) {
-        int index = charToIndex(c);
-        return index!=-1 ? accentByCodePoint[index] : 0;
-    }
-    
-    private int charToIndex(char c) {
-        int index = c - MIN_CODEPOINT;
-        return (index>=0 && index<MAX_CODEPOINT) ? index : -1;
-    }
+	private final String mathVariantName;
+	private final char[] accentByCodePoint;
+
+	public MathVariantMap(final String mathVariantName, final char[] accentData) {
+		this.mathVariantName = mathVariantName;
+		this.accentByCodePoint = new char[MAX_CODEPOINT - MIN_CODEPOINT];
+		char ascii, accented;
+		int index;
+		int length = accentData.length - 1;
+		for (int i = 0; i < length;) {
+			ascii = accentData[i++];
+			accented = accentData[i++];
+			index = charToIndex(ascii);
+			if (index != -1) {
+				accentByCodePoint[index] = accented;
+			}
+		}
+	}
+
+	public String getMathVariantName() {
+		return mathVariantName;
+	}
+
+	public char getAccentedChar(char c) {
+		int index = charToIndex(c);
+		return index != -1 ? accentByCodePoint[index] : 0;
+	}
+
+	private int charToIndex(char c) {
+		int index = c - MIN_CODEPOINT;
+		return (index >= 0 && index < MAX_CODEPOINT) ? index : -1;
+	}
 
 }
