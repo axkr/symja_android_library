8d6594aac4fe6d4befaeca9cc67c075b9d7fa134
axexlck <axelclk@gmail.com>
2020-12-13 19:03:40 +0100

    Beep.java

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/Beep.java
@@ -0,0 +1,36 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.interfaces.AbstractEvaluator;
+import org.matheclipse.core.eval.interfaces.IFunctionEvaluator;
+import org.matheclipse.core.expression.S;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.ISymbol;
+
+public class Beep extends AbstractEvaluator {
+
+  public Beep() {}
+
+  @Override
+  public IExpr evaluate(final IAST ast, EvalEngine engine) {
+    String s = System.getProperty("os.name");
+    if (s.contains("Windows")) {
+      // this should work on Windows using awt:
+      java.awt.Toolkit.getDefaultToolkit().beep();
+    } else {
+      System.out.print("\007"); // \007 is the ASCII bell
+      System.out.flush();
+    }
+    return S.Null;
+  }
+
+  public int[] expectedArgSize(IAST ast) {
+    return IFunctionEvaluator.ARGS_0_0;
+  }
+
+  @Override
+  public void setUp(final ISymbol newSymbol) {
+    // newSymbol.setAttributes(ISymbol.LISTABLE | ISymbol.NUMERICFUNCTION);
+  }
+}
