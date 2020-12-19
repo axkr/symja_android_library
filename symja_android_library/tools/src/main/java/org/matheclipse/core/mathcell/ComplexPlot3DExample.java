c079eed0d8a0b52a32d01460238fb85dfd08608f
axexlck <axelclk@gmail.com>
2020-12-19 19:21:07 +0100

    Fix ComplexPlot3D example

--- symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/ComplexPlot3DExample.java
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/ComplexPlot3DExample.java
@@ -11,7 +11,7 @@ import org.matheclipse.parser.client.SyntaxError;
 public class ComplexPlot3DExample extends BasePlotExample {
   @Override
   public String exampleFunction() {
-    return "ComplexPlot3D(StruveH(z), {z, -4.999-4.9999*I,4.9999+4.9999*I}, PlotRange->{-5.0,10.0}) ";
+    return "ComplexPlot3D(StruveH(2, z), {z, -4.999-4.9999*I,4.9999+4.9999*I}, PlotRange->{-5.0,10.0}) ";
     //		return "ComplexPlot3D((z^2 + 1)/(z^2 - 1),  {z, -2 - 2*I, 2 + 2*I}, PlotRange->{0,3}) ";
   }
 
