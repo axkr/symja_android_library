da9727e8630da738c30a12cf8685cbd74c1a0299
axexlck <axelclk@gmail.com>
2020-12-19 19:23:29 +0100

    FormulaExample

--- symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/FormulaExample.java
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/FormulaExample.java
@@ -12,8 +12,8 @@ public class FormulaExample extends BasePlotExample {
 
   @Override
   public String exampleFunction() {
-    return "Manipulate(-0.07 *x^5-0.42*x^4+0.94*x^3-4.25*x^2+86.5, {x,4.873,4.874})";
-    //		return "Manipulate(Factor(x^n + 1), {n, 1, 5, 1})";
+    //    return "Manipulate(-0.07 *x^5-0.42*x^4+0.94*x^3-4.25*x^2+86.5, {x,4.873,4.874})";
+    return "Manipulate(Factor(x^n + 1), {n, 1, 5, 1})";
   }
 
   public static void main(String[] args) {
