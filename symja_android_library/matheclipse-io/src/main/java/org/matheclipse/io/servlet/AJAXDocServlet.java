ecd530f57af34d856fec868d35cce5467c51f8c4
axexlck <axelclk@gmail.com>
2021-02-20 13:24:59 +0100

    Caching in AJAXDocServlet

--- symja_android_library/matheclipse-io/src/main/java/org/matheclipse/io/servlet/AJAXDocServlet.java
+++ symja_android_library/matheclipse-io/src/main/java/org/matheclipse/io/servlet/AJAXDocServlet.java
@@ -21,12 +21,20 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.text.StringEscapeUtils;
 import org.commonmark.Extension;
 import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.CustomNode;
+import org.commonmark.node.Delimited;
 import org.commonmark.node.FencedCodeBlock;
 import org.commonmark.node.IndentedCodeBlock;
 import org.commonmark.node.Link;
 import org.commonmark.node.Node;
+import org.commonmark.node.Text;
 import org.commonmark.parser.Parser;
+import org.commonmark.parser.delimiter.DelimiterProcessor;
+import org.commonmark.parser.delimiter.DelimiterRun;
 import org.commonmark.renderer.NodeRenderer;
+import org.commonmark.renderer.html.AttributeProvider;
+import org.commonmark.renderer.html.AttributeProviderContext;
+import org.commonmark.renderer.html.AttributeProviderFactory;
 import org.commonmark.renderer.html.CoreHtmlNodeRenderer;
 import org.commonmark.renderer.html.HtmlNodeRendererContext;
 import org.commonmark.renderer.html.HtmlNodeRendererFactory;
@@ -37,6 +45,7 @@ import org.matheclipse.core.expression.Context;
 import org.matheclipse.core.expression.F;
 import org.matheclipse.core.form.Documentation;
 import org.matheclipse.core.form.output.WolframFormFactory;
+import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.ISymbol;
 import org.matheclipse.core.parser.ExprParser;
@@ -45,91 +54,82 @@ import org.matheclipse.parser.client.SyntaxError;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 public class AJAXDocServlet extends HttpServlet {
+  private static final Cache<String, String> JSON_DOCS_CACHE =
+      CacheBuilder.newBuilder().maximumSize(100).build();
+
+  private static final long serialVersionUID = 4636252666864898484L;
+
   public static final String FUNCTIONS_PREFIX1 = "/functions/";
   public static final String FUNCTIONS_PREFIX2 = "functions/";
 
   public static final ObjectMapper JSON_OBJECT_MAPPER = new ObjectMapper();
 
   /** A TeX node containing text and other inline nodes as children. */
-  //  private static class TeX extends CustomNode implements Delimited {
-  //
-  //    private static final String DELIMITER = "$";
-  //
-  //    @Override
-  //    public String getOpeningDelimiter() {
-  //      return DELIMITER;
-  //    }
-  //
-  //    @Override
-  //    public String getClosingDelimiter() {
-  //      return DELIMITER;
-  //    }
-  //  }
-  //
-  //  private static class TeXDelimiterProcessor implements DelimiterProcessor {
-  //
-  //    @Override
-  //    public char getOpeningCharacter() {
-  //      return '$';
-  //    }
-  //
-  //    @Override
-  //    public char getClosingCharacter() {
-  //      return '$';
-  //    }
-  //
-  //    @Override
-  //    public int getMinLength() {
-  //      return 2;
-  //    }
-  //
-  //    @Override
-  //    public int getDelimiterUse(DelimiterRun opener, DelimiterRun closer) {
-  //      if (opener.length() >= 2 && closer.length() >= 2) {
-  //        // Use exactly two delimiters even if we have more, and don't care about internal
-  //        // openers/closers.
-  //        return 2;
-  //      } else {
-  //        return 0;
-  //      }
-  //    }
-  //
-  //    @Override
-  //    public void process(Text opener, Text closer, int delimiterCount) {
-  //      // Wrap nodes between delimiters in TeX.
-  //      Node teX = new TeX();
-  //
-  //      Node tmp = opener.getNext();
-  //      while (tmp != null && tmp != closer) {
-  //        Node next = tmp.getNext();
-  //        teX.appendChild(tmp);
-  //        tmp = next;
-  //      }
-  //
-  //      opener.insertAfter(teX);
-  //    }
-  //  }
-  //
-  //  private static class TeXExtension implements Parser.ParserExtension {
-  //
-  //    private TeXExtension() {}
-  //
-  //    public static Extension create() {
-  //      return new TeXExtension();
-  //    }
-  //
-  //    @Override
-  //    public void extend(Parser.Builder parserBuilder) {
-  //      parserBuilder.customDelimiterProcessor(new TeXDelimiterProcessor());
-  //    }
-  //  }
+  private static class TeX extends CustomNode implements Delimited {
+
+    private static final String DELIMITER = "$";
+
+    @Override
+    public String getOpeningDelimiter() {
+      return DELIMITER;
+    }
+
+    @Override
+    public String getClosingDelimiter() {
+      return DELIMITER;
+    }
+  }
+
+  private static class TeXDelimiterProcessor implements DelimiterProcessor {
+
+    @Override
+    public char getOpeningCharacter() {
+      return '$';
+    }
+
+    @Override
+    public char getClosingCharacter() {
+      return '$';
+    }
+
+    @Override
+    public int getMinLength() {
+      return 2;
+    }
+
+    @Override
+    public int process(DelimiterRun opener, DelimiterRun closer) {
+      if (opener.length() >= 2 && closer.length() >= 2) {
+        // Use exactly two delimiters even if we have more, and don't care about internal
+        // openers/closers.
+        return 2;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  private static class TeXExtension implements Parser.ParserExtension {
+
+    private TeXExtension() {}
+
+    public static Extension create() {
+      return new TeXExtension();
+    }
+
+    @Override
+    public void extend(Parser.Builder parserBuilder) {
+      parserBuilder.customDelimiterProcessor(new TeXDelimiterProcessor());
+    }
+  }
 
   static class DocNodeRenderer extends CoreHtmlNodeRenderer {
     private final HtmlWriter html;
-    private boolean inHeader = false;
-
+    //    private boolean inHeader=false;
     public DocNodeRenderer(HtmlNodeRendererContext context) {
       super(context);
       this.html = context.getWriter();
@@ -141,6 +141,7 @@ public class AJAXDocServlet extends HttpServlet {
       Set<Class<? extends Node>> set = new HashSet<Class<? extends Node>>();
       set.add(FencedCodeBlock.class);
       set.add(Link.class);
+      set.add(TeX.class);
       return set;
     }
 
@@ -150,6 +151,8 @@ public class AJAXDocServlet extends HttpServlet {
         fencedCodeBlock((FencedCodeBlock) node);
       } else if (node instanceof Link) {
         link((Link) node);
+      } else if (node instanceof TeX) {
+        tex((TeX) node);
       } else {
         IndentedCodeBlock codeBlock = (IndentedCodeBlock) node;
         html.line();
@@ -160,7 +163,7 @@ public class AJAXDocServlet extends HttpServlet {
       }
     }
 
-    public void fencedCodeBlock(FencedCodeBlock fencedCodeBlock) {
+    private void fencedCodeBlock(FencedCodeBlock fencedCodeBlock) {
 
       String literal = fencedCodeBlock.getLiteral();
       WolframFormFactory wolframForm = WolframFormFactory.get();
@@ -223,7 +226,7 @@ public class AJAXDocServlet extends HttpServlet {
       }
     }
 
-    public void link(Link link) {
+    private void link(Link link) {
       String destination = link.getDestination();
       int index = destination.indexOf(".md");
       if (index > 0) {
@@ -257,6 +260,19 @@ public class AJAXDocServlet extends HttpServlet {
       }
     }
 
+    private void tex(TeX teXNode) {
+      Text text = (Text) teXNode.getFirstChild();
+      html.raw(text.getLiteral());
+      //      StringBuilder buf = new StringBuilder();
+      //      buf.append(
+      //          "<math xmlns=\"http://www.w3.org/1998/Math/MathML\"
+      // xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+      //              + "  <mstyle displaystyle=\"true\" mathvariant=\"sans-serif\">\n");
+      //      buf.append(text.getLiteral());
+      //      buf.append("</mstyle>\n" + "</math>");
+      //      html.raw(buf.toString());
+    }
+
     //    @Override
     //    public void visit(CustomBlock customBlock) {
     //      if (customBlock instanceof TableBlock) {
@@ -269,44 +285,38 @@ public class AJAXDocServlet extends HttpServlet {
     //
     //    @Override
     //    public void visit(CustomNode customNode) {
-    //      if (customNode instanceof TableHead) {
-    //        inHeader = true;
-    //        visitChildren(customNode);
+    //          if (customNode instanceof TableHead) {
+    //            inHeader = true;
+    //            visitChildren(customNode);
     //
-    //      } else if (customNode instanceof TableBody) {
-    //        inHeader = false;
-    //        visitChildren(customNode);
+    //          } else if (customNode instanceof TableBody) {
+    //            inHeader = false;
+    //            visitChildren(customNode);
     //
-    //      } else if (customNode instanceof org.commonmark.ext.gfm.tables.TableRow) {
-    //        visitChildren(customNode);
-    //      } else if (customNode instanceof TableCell) {
-    //        TableCell cell = (TableCell) customNode;
-    //        visitChildren(cell);
-    //        //  if (inHeader) {
-    //        //
-    //        // }
-    //      } else if (customNode instanceof TeX) {
+    //          } else if (customNode instanceof org.commonmark.ext.gfm.tables.TableRow) {
+    //            visitChildren(customNode);
+    //          } else if (customNode instanceof TableCell) {
+    //            TableCell cell = (TableCell) customNode;
+    //            visitChildren(cell);
+    //            //  if (inHeader) {
+    //            //
+    //            // }
+    //          } else
+    //      if (customNode instanceof TeX) {
     //        visitTeXNode((TeX) customNode);
     //      } else {
-    //        visitChildren(customNode);
+    //        super.visit(customNode);
     //      }
     //    }
-    //
-    //    private void visitTeXNode(TeX teXNode) {
-    //      System.out.println("content = " + teXNode.toString());
-    //    }
-    //
-    //    private void visitTableBlock(TableBlock tableBlock) {
-    //      try {
-    //        visitChildren(tableBlock);
-    //      } catch (Exception e) {
-    //        e.printStackTrace();
-    //      }
-    //    }
-  }
+    //        private void visitTableBlock(TableBlock tableBlock) {
+    //          try {
+    //            visitChildren(tableBlock);
+    //          } catch (Exception e) {
+    //            e.printStackTrace();
+    //          }
+    //        }
 
-  /** */
-  private static final long serialVersionUID = -7389567393700726482L;
+  }
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse res)
@@ -333,28 +343,34 @@ public class AJAXDocServlet extends HttpServlet {
         }
       }
 
-      StringBuilder markdownBuf = new StringBuilder(1024);
-      printMarkdown(markdownBuf, value);
-      String markdownStr = markdownBuf.toString().trim();
-      if (markdownStr.length() > 0) {
-        String html = generateHTMLString(markdownBuf.toString());
-        StringBuilder htmlBuf = new StringBuilder(1024);
-        htmlBuf.append("<div id=\"docContent\">\n");
-        htmlBuf.append("<div id=\"mjax\">\n");
-        htmlBuf.append(html);
-        htmlBuf.append("\n</div>");
-        // see https://docs.mathjax.org/en/v2.7-latest/advanced/typeset.html -
-        // Modifying Math on the  Page
-        //        htmlBuf.append(
-        //            "<script
-        // type=\"text/javascript\">MathJax.Hub.Queue(['Typeset',MathJax.Hub,'mjax']); </script>");
-        //        htmlBuf.append("\n</div>");
-
-        out.println(createJSONDocString(htmlBuf.toString()));
+      String jsonStr = JSON_DOCS_CACHE.getIfPresent(value);
+      if (jsonStr != null) {
+        out.println(jsonStr);
       } else {
-        out.println(
-            createJSONDocString(
-                "<p>Insert a keyword and append a '*' to search for keywords. Example: <b>Int*</b>.</p>"));
+        StringBuilder markdownBuf = new StringBuilder(1024);
+        printMarkdown(markdownBuf, value);
+        String markdownStr = markdownBuf.toString().trim();
+        if (markdownStr.length() > 0) {
+          String html = generateHTMLString(markdownBuf.toString());
+          StringBuilder htmlBuf = new StringBuilder(1024);
+          htmlBuf.append("<div id=\"docContent\">\n");
+          htmlBuf.append("<div id=\"mjax\">\n");
+          htmlBuf.append(html);
+          htmlBuf.append("\n</div>");
+          // see https://docs.mathjax.org/en/v2.7-latest/advanced/typeset.html -
+          // Modifying Math on the  Page
+          htmlBuf.append(
+              "<script type=\"text/javascript\">MathJax.Hub.Queue(['Typeset',MathJax.Hub,'mjax']); </script>");
+          htmlBuf.append("\n</div>");
+
+          jsonStr = createJSONDocString(htmlBuf.toString());
+          JSON_DOCS_CACHE.put(value, jsonStr);
+          out.println(jsonStr);
+        } else {
+          out.println(
+              createJSONDocString(
+                  "<p>Insert a keyword and append a '*' to search for keywords. Example: <b>Int*</b>.</p>"));
+        }
       }
     } catch (Exception e) {
       // ...
@@ -364,13 +380,16 @@ public class AJAXDocServlet extends HttpServlet {
   public static String generateHTMLString(final String markdownStr) {
     List<Extension> EXTENSIONS =
         Arrays.asList( //
-            //            TeXExtension.create(), //
-            TablesExtension.create());
-    Parser parser = Parser.builder().extensions(EXTENSIONS).build();
+            TeXExtension.create(), TablesExtension.create());
+    Parser parser =
+        Parser.builder() //
+            .extensions(EXTENSIONS)
+            .build();
     Node document = parser.parse(markdownStr);
 
     HtmlRenderer renderer =
-        HtmlRenderer.builder()
+        HtmlRenderer.builder() //
+            .extensions(EXTENSIONS)
             .nodeRendererFactory(
                 new HtmlNodeRendererFactory() {
                   @Override
@@ -392,6 +411,9 @@ public class AJAXDocServlet extends HttpServlet {
     try {
       InputStream is = classloader.getResourceAsStream(fileName);
       if (is != null) {
+        // jump back to Main documentation page
+        out.append("\n\n [&larr; Main](index.md)\n");
+
         final BufferedReader f = new BufferedReader(new InputStreamReader(is, "UTF-8"));
         String line;
         while ((line = f.readLine()) != null) {
