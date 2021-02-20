28b8216b99319db6c6f9a4f22f920c1b94e4b202
axexlck <axelclk@gmail.com>
2021-02-20 14:16:43 +0100

    PlotSurdExample

---
+++ symja_android_library/tools/src/main/java/org/matheclipse/core/mathcell/PlotSurdExample.java
@@ -0,0 +1,15 @@
+package org.matheclipse.core.mathcell;
+
+public class PlotSurdExample extends BasePlotExample {
+
+  @Override
+  public String exampleFunction() { 
+
+        return "Plot((1 + x^3)*CubeRoot(x), {x, -1, 1})"; 
+  }
+
+  public static void main(String[] args) {
+    PlotSurdExample p = new PlotSurdExample();
+    p.generateHTML();
+  }
+}
