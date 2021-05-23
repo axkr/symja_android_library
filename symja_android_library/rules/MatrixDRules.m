6c0cd2fe09f1130751504f3d147000dd149b213a
axexlck <axelclk@gmail.com>
2021-05-23 11:35:03 +0200

    #224 WIP some ideas for MatrixD rules
    some example rules form the matrix cookbook:
    - https://archive.org/details/imm3274
    
    TODO
    - improve assumption system for domains: Arrays, Vectors, Matrices
    - TensorRank() must test assumptions of the form:
      $Assumptions=Element(X, Matrices({n,n},Complexes))

---
+++ symja_android_library/rules/MatrixDRules.m
@@ -0,0 +1,26 @@
+{
+(* Rule 40 of https://archive.org/details/imm3274 *)
+MatrixD(Inverse(x_),x_?(TensorRank(x)==2)) := (-Inverse(x)).MatrixD(a, x).Inverse(x),
+
+(* Rule 44 *)
+MatrixD(Transpose(x_),x_?(TensorRank(x)==2)) := Transpose(MatrixD(x, x)),
+
+(* Rule 49 *)
+MatrixD(Det(x_),x_?(TensorRank(x)==2)) := Det(x)*Transpose(Inverse(x)),
+
+(* Rule 51 *)
+MatrixD(Det(a_?(TensorRank(x)==2) . x_ . b_?(TensorRank(x)==2)), x_?(TensorRank(x)==2)) := Det(a.x.b)*Transpose(Inverse(x)),
+
+(* Rule 52 x square, invertible *)
+MatrixD(Det(Transpose(x_) . a_?(TensorRank(x)==2) . x_), x_?(TensorRank(x)==2)) := 2*Det(Transpose(x).a.x)*Inverse(Transpose(x)),
+
+(* Rule 55 *)
+MatrixD(Log(Det(Transpose(x_).x_)), x_?(TensorRank(x)==2)) := 2*Inverse(Transpose(x)),
+
+(* Rule 57 *)
+MatrixD(Log(Det(x_)), x_?(TensorRank(x)==2)) := Inverse(Transpose(x)),
+
+(* Rule 62 *)s
+MatrixD(Det(Inverse(x_)),x_?(TensorRank(x)==2)) := (-Det(Inverse(x)))*Transpose(Inverse(x))
+  
+} 
