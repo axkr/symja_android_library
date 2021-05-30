0d94f06edaf93934996e0c2596599df78c9ba8e0
axexlck <axelclk@gmail.com>
2021-05-30 14:27:59 +0200

    #224 WIP First MatrixD version

---
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/reflection/system/MatrixD.java
@@ -0,0 +1,168 @@
+package org.matheclipse.core.reflection.system;
+
+import org.matheclipse.core.builtin.IOFunctions;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.eval.exception.ValidateException;
+import org.matheclipse.core.eval.interfaces.AbstractFunctionEvaluator;
+import org.matheclipse.core.expression.ASTSeriesData;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.core.expression.S;
+import org.matheclipse.core.generic.BinaryBindIth1st;
+import org.matheclipse.core.interfaces.IAST;
+import org.matheclipse.core.interfaces.IASTAppendable;
+import org.matheclipse.core.interfaces.IASTMutable;
+import org.matheclipse.core.interfaces.IExpr;
+import org.matheclipse.core.interfaces.IInteger;
+import org.matheclipse.core.interfaces.IRational;
+import org.matheclipse.core.interfaces.ISymbol;
+import org.matheclipse.core.reflection.system.rules.DRules;
+import org.matheclipse.core.reflection.system.rules.MatrixDRules;
+
+public class MatrixD extends AbstractFunctionEvaluator implements MatrixDRules {
+
+  public MatrixD() {}
+
+  @Override
+  public IAST getRuleAST() {
+    return RULES;
+  }
+
+  /**
+   * For the referenced formula numbers (XX) see: <a
+   * href="https://archive.org/details/imm3274/">Internet Archive - The Matrix Cookbook</a>
+   */
+  @Override
+  public IExpr evaluate(final IAST ast, EvalEngine engine) {
+    if (ast.size() < 3) {
+      return F.NIL;
+    }
+    try {
+      final IExpr fx = ast.arg1();
+      if (fx.isIndeterminate()) {
+        return S.Indeterminate;
+      }
+      if (ast.size() > 3) {
+        // reduce arguments by folding MatrixD[fxy, x, y] to MatrixD[ MatrixD[fxy, x], y] ...
+        return ast.foldLeft((x, y) -> engine.evaluateNIL(F.MatrixD(x, y)), fx, 2);
+      }
+
+      IExpr x = ast.arg2();
+      if (!(x.isVariable() || x.isList())) {
+        // `1` is not a valid variable.
+        return IOFunctions.printMessage(ast.topHead(), "ivar", F.List(x), engine);
+      }
+
+      if (fx.isList()) {
+        IAST list = (IAST) fx;
+        // thread over first list
+        return list.mapThreadEvaled(engine, F.ListAlloc(list.size()), ast, 1);
+      }
+
+      if (x.isList()) {
+        // MatrixD[fx_, {...}]
+        IAST xList = (IAST) x;
+        if (xList.isAST1() && xList.arg1().isListOfLists()) {
+          IAST subList = (IAST) xList.arg1();
+          IASTAppendable result = F.ListAlloc(subList.size());
+          result.appendArgs(subList.size(), i -> F.MatrixD(fx, F.List(subList.get(i))));
+          return result;
+        } else if (xList.isAST1() && xList.arg1().isList()) {
+          IAST subList = (IAST) xList.arg1();
+          return subList.mapLeft(F.ListAlloc(), (a, b) -> engine.evaluateNIL(F.MatrixD(a, b)), fx);
+        } else if (xList.isAST2()) {
+          if (xList.arg1().isList()) {
+            x = F.List(xList.arg1());
+          } else {
+            x = xList.arg1();
+          }
+          IExpr arg2 = xList.arg2();
+          int n = arg2.toIntDefault();
+          if (n >= 0) {
+            IExpr temp = fx;
+            for (int i = 0; i < n; i++) {
+              temp = S.MatrixD.ofNIL(engine, temp, x);
+              if (!temp.isPresent()) {
+                return F.NIL;
+              }
+            }
+            return temp;
+          }
+          if (arg2.isFree(num -> num.isNumber(), false)) {
+            if (fx.equals(x)) {
+              return S.$SingleEntryMatrix;
+            }
+            if (fx.isAST()) {
+              final IAST function = (IAST) fx;
+              if (function.isPlus()) {
+                // MatrixD(a_+b_+c_,x_) -> MatrixD(a,x)+MatrixD(b,x)+MatrixD(c,x)
+                return function.mapThread(F.MatrixD(F.Slot1, xList), 1); // (35)
+              }
+            }
+            return F.NIL;
+          }
+          if (!x.isVariable()) {
+            // `1` is not a valid variable.
+            return IOFunctions.printMessage(ast.topHead(), "ivar", F.List(x), engine);
+          }
+          if (arg2.isAST()) {
+            return F.NIL;
+          }
+          // Multiple derivative specifier `1` does not have the form {variable, n} where n is a
+          // symbolic expression or a non-negative integer.
+          return IOFunctions.printMessage(ast.topHead(), "dvar", F.List(xList), engine);
+        }
+        return F.NIL;
+      }
+
+      if (!x.isVariable()) {
+        // `1` is not a valid variable.
+        return IOFunctions.printMessage(ast.topHead(), "ivar", F.List(x), engine);
+      }
+
+      return binaryMatrixD(fx, x);
+    } catch (final ValidateException ve) {
+      // int number validation
+      return engine.printMessage(ve.getMessage(ast.topHead()));
+    }
+  }
+
+  /**
+   * Evaluate <code>MatrixD(functionO>fX, x)</code> for some general cases.
+   *
+   * <p>Rule numbers are from <a href="https://archive.org/details/imm3274">Internet Archive - The
+   * Matrix Cookbook</a>
+   *
+   * @param functionOfX the function of <code>x</code>
+   * @param x derive w.r.t this variable
+   * @return
+   */
+  private static IExpr binaryMatrixD(final IExpr functionOfX, IExpr x) {
+    if (functionOfX.isFree(x, true)) {
+      return F.C0; // (33)
+    }
+
+    if (functionOfX.equals(x)) {
+      // MatrixD[x_,x_] -> $SingleEntryMatrix
+      return S.$SingleEntryMatrix;
+    }
+
+    if (functionOfX.isAST() && functionOfX.size() >= 2) {
+      final IAST function = (IAST) functionOfX;
+      //      final IExpr arg1 = function.arg1();
+      if (function.isPlus()) {
+        // MatrixD(a_+b_+c_,x_) -> MatrixD(a,x)+MatrixD(b,x)+MatrixD(c,x)
+        return function.mapThread(F.MatrixD(F.Slot1, x), 1); // (35)
+      } else if (function.isTimes() // (38)
+          || function.isASTSizeGE(S.Dot, 3) // (37)
+          || function.isASTSizeGE(S.KroneckerProduct, 3)) { // (39)
+        return function.map(F.PlusAlloc(16), new BinaryBindIth1st(function, F.MatrixD(S.Null, x)));
+      }
+    }
+    return F.NIL;
+  }
+
+  @Override
+  public int[] expectedArgSize(IAST ast) {
+    return ARGS_2_INFINITY;
+  }
+}
---
+++ symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/MatrixDTest.java
@@ -0,0 +1,107 @@
+package org.matheclipse.io.system;
+
+import org.matheclipse.core.basic.Config;
+import org.matheclipse.core.basic.ToggleFeature;
+import org.matheclipse.core.eval.EvalEngine;
+import org.matheclipse.core.expression.F;
+import org.matheclipse.script.engine.MathScriptEngine;
+
+public class MatrixDTest extends AbstractTestCase {
+
+  public MatrixDTest(String name) {
+    super(name);
+  }
+
+  public void testMatrixD001() {
+
+    // bad input 1
+    check(
+        "MatrixD(-2147483648,Slot(\"\"),{{0,0},2147483647},{x,-1/2,1/2,Graph({1->2,2->3,3->1})}, 7*19,DirectedInfinity({0,0,0}),(-2147483648)^2,2*{1,2,3})", //
+        "MatrixD(-2147483648,Slot(),{{0,0},2147483647},{x,-1/2,1/2,Graph({1,2,3},{1->2,2->3,3->1})},\n"
+            + "133,DirectedInfinity({0,0,0}),4611686018427387904,{2,4,6})");
+    // bad input 2
+    check(
+        "MatrixD({1},SparseArray({{1,0},{0,1}})*Sqrt(5),2*(-3/2),OptionValue(OptionValue(x)),{{0,0},10007},OptionValue(-3/2),(3/2)[[1]],-I)", //
+        "MatrixD({1},SparseArray(Number of elements: 2 Dimensions: {2,2} Default value: 0),-\n"
+            + "3,OptionValue(OptionValue(x)),{{0,0},10007},OptionValue(-3/2),(3/2)[[1]],-I)");
+    // bad input 3
+    check(
+        "MatrixD((Heads->False)*Pi,x[[{3/4}]],\"\",2+I,Null,-1,{{1,0},10007},ComplexInfinity,2-I)", //
+        "MatrixD(Pi*(Heads->False),x[[{3/4}]],,2+I,Null,-1,{{1,0},10007},ComplexInfinity,\n"
+            + "2-I)");
+    check(
+        "$Assumptions={Element(X, Matrices({n,n},Complexes)),Element(A, Matrices({n,n},Complexes)),Element(B, Matrices({4,4},Complexes))}", //
+        "{X∈Matrices({n,n},Complexes),A∈Matrices({n,n},Complexes),B∈Matrices({4,4},Complexes)}");
+
+    check(
+        "MatrixD(X,X)", //
+        "$SingleEntryMatrix");
+    check(
+        "MatrixD(A.X, X)", //
+        "0.X+A.$SingleEntryMatrix");
+    check(
+        "MatrixD(Det(MatrixPower(X, k)), X)", //
+        "k*Det(MatrixPower(X,k))*Inverse(Transpose(X))");
+    check(
+        "MatrixD(Inverse(X), X)", //
+        "(-Inverse(X)).$SingleEntryMatrix.Inverse(X)");
+  }
+
+  public void testMatrixDet001() {
+    check(
+        "$Assumptions={Element(X, Matrices({n,n},Complexes)),Element(A, Matrices({n,n},Complexes)),Element(B, Matrices({4,4},Complexes))}", //
+        "{X∈Matrices({n,n},Complexes),A∈Matrices({n,n},Complexes),B∈Matrices({4,4},Complexes)}");
+
+    check(
+        "TensorRank(X)", //
+        "2");
+    check(
+        "MatrixD(Transpose(X), X)", //
+        "Transpose($SingleEntryMatrix)");
+    check(
+        "MatrixD(Det(X), X)", //
+        "Det(X)*Transpose(Inverse(X))");
+    check(
+        "MatrixD(Det(A.X.B), X)", //
+        "Det(A.X.B)*Transpose(Inverse(X))");
+    check(
+        "MatrixD(Det(Inverse(X)),X)", //
+        "-Det(Inverse(X))*Transpose(Inverse(X))");
+    check(
+        "MatrixD(Log(Det(Transpose(X).X)), X)", // (55)
+        "2*Inverse(Transpose(X))");
+    check(
+        "MatrixD(Log @ Det(X), X)", // (57)
+        "Inverse(Transpose(X))");
+  }
+
+//  public void testMatrixD002() {
+//    check(
+//        "$Assumptions={Element(X, Matrices({n,n},Complexes)),Element(A, Matrices({n,n},Complexes)),Element(B, Matrices({4,4},Complexes))}", //
+//        "{X∈Matrices({n,n},Complexes),A∈Matrices({n,n},Complexes),B∈Matrices({4,4},Complexes)}");
+//    check(
+//        "MatrixD(A, X)",  
+//        "0");
+//    check(
+//            "MatrixD(a*X, X)",  
+//            "$SingleEntryMatrix*a");
+//    check(
+//        "MatrixD(Transpose(X)*A*x + c*Transpose(sin(y))*X, X)",  
+//        "");
+//  }
+  
+  /** The JUnit setup method */
+  @Override
+  protected void setUp() {
+    super.setUp();
+    Config.SHORTEN_STRING_LENGTH = 1024;
+    Config.MAX_AST_SIZE = 1000000;
+    EvalEngine.get().setIterationLimit(50000);
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    Config.SHORTEN_STRING_LENGTH = 80;
+  }
+}
