5ca3255401ca4a6f130790ebb41f480112948c98
Hannes Wellmann <44067969+HannesWell@users.noreply.github.com>
2021-06-16 14:25:38 +0200

    [Tests] improve and speed up ArchUnit tests
    + use static fields annotated with @ArchTest and @AnalyzeClasses to
    define all rules.
     - to enable this ArchUnitTests simply operatates on all classes in any
     "org" package of this library.
     - this cuts the runtime approximatly to one third because the classes
     are cached between tests
     See https://www.archunit.org/userguide/html/000_Index.html#_junit_support
    
    + disable failing tests instead of commenting them out

--- symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/CodingRulesTest.java
+++ symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/CodingRulesTest.java
@@ -1,15 +1,4 @@
 package org.matheclipse.io.system.archunit;
-  
-
-import java.util.logging.Logger;
-
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.junit.ArchUnitRunner;
-import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.CompositeArchRule;
-import org.junit.runner.RunWith;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
@@ -19,43 +8,55 @@ import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
 import static com.tngtech.archunit.library.GeneralCodingRules.NO_CLASSES_SHOULD_USE_JODATIME;
+import java.util.logging.Logger;
+import org.junit.runner.RunWith;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchIgnore;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.CompositeArchRule;
 
 @RunWith(ArchUnitRunner.class)
 @AnalyzeClasses(packages = "org.matheclipse")
 public class CodingRulesTest {
 
-//    @ArchTest
-//    private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
+  @ArchIgnore
+  @ArchTest
+  private final ArchRule no_access_to_standard_streams = NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS;
 
-//    @ArchTest
-//    private void no_access_to_standard_streams_as_method(JavaClasses classes) {
-//        noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
-//    }
-//
-//    @ArchTest
-//    private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
+  @ArchIgnore
+  @ArchTest
+  private void no_access_to_standard_streams_as_method(JavaClasses classes) {
+    noClasses().should(ACCESS_STANDARD_STREAMS).check(classes);
+  }
 
-    @ArchTest
-    private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
+  @ArchIgnore
+  @ArchTest
+  private final ArchRule no_generic_exceptions = NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS;
 
-    @ArchTest
-    private final ArchRule loggers_should_be_private_static_final =
-            fields().that().haveRawType(Logger.class)
-                    .should().bePrivate()
-                    .andShould().beStatic()
-                    .andShould().beFinal()
-                    .because("we agreed on this convention");
+  @ArchTest
+  private final ArchRule no_java_util_logging = NO_CLASSES_SHOULD_USE_JAVA_UTIL_LOGGING;
 
-    @ArchTest
-    private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
+  @ArchTest
+  private final ArchRule loggers_should_be_private_static_final =
+      fields().that().haveRawType(Logger.class) //
+          .should().bePrivate() //
+          .andShould().beStatic() //
+          .andShould().beFinal() //
+          .because("we agreed on this convention");
 
-    @ArchTest
-    private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
+  @ArchTest
+  private final ArchRule no_jodatime = NO_CLASSES_SHOULD_USE_JODATIME;
 
-//    @ArchTest
-//    public static final ArchRule no_classes_should_access_standard_streams_or_throw_generic_exceptions =
-//            CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS)
-//                    .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS);
+  @ArchTest
+  private final ArchRule no_field_injection = NO_CLASSES_SHOULD_USE_FIELD_INJECTION;
 
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_classes_should_access_standard_streams_or_throw_generic_exceptions =
+      CompositeArchRule.of(NO_CLASSES_SHOULD_ACCESS_STANDARD_STREAMS)
+          .and(NO_CLASSES_SHOULD_THROW_GENERIC_EXCEPTIONS);
 }
 
--- symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/CyclicDependencyRulesTest.java
+++ symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/CyclicDependencyRulesTest.java
@@ -1,73 +1,78 @@
 package org.matheclipse.io.system.archunit;
 
-import com.tngtech.archunit.base.DescribedPredicate;
+
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import org.junit.runner.RunWith;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchIgnore;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.library.dependencies.SliceAssignment;
 import com.tngtech.archunit.library.dependencies.SliceIdentifier;
-import org.junit.runner.RunWith;
-
-import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
-import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 @RunWith(ArchUnitRunner.class)
 @AnalyzeClasses(packages = "org.matheclipse")
 public class CyclicDependencyRulesTest {
 
-//    @ArchTest
-//    public static final ArchRule no_cycles_by_method_calls_between_slices =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_by_constructor_calls_between_slices =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_by_inheritance_between_slices =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_by_field_access_between_slices =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_by_member_dependencies_between_slices =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_in_simple_scenario =
-//            slices().matching("..org.matheclipse.(*)..").namingSlices("$1").should().beFreeOfCycles();
-//
-//    @ArchTest
-//    public static final ArchRule no_cycles_in_complex_scenario =
-//            slices().matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_by_method_calls_between_slices = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
 
-//    @ArchTest
-//    public static final ArchRule no_cycles_in_freely_customized_slices =
-//            slices().assignedFrom(inComplexSliceOneOrTwo())
-//                    .namingSlices("$1[$2]")
-//                    .should().beFreeOfCycles();
-//
-//    private static SliceAssignment inComplexSliceOneOrTwo() {
-//        return new SliceAssignment() {
-//            @Override
-//            public String getDescription() {
-//                return "complex slice one or two";
-//            }
-//
-//            @Override
-//            public SliceIdentifier getIdentifierOf(JavaClass javaClass) {
-//                if (javaClass.getPackageName().contains("complexcycles.slice1")) {
-//                    return SliceIdentifier.of("Complex-Cycle", "One");
-//                }
-//                if (javaClass.getPackageName().contains("complexcycles.slice2")) {
-//                    return SliceIdentifier.of("Complex-Cycle", "Two");
-//                }
-//                return SliceIdentifier.ignore();
-//            }
-//        };
-//    }
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_by_constructor_calls_between_slices = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_by_inheritance_between_slices = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_by_field_access_between_slices = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_by_member_dependencies_between_slices = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_in_simple_scenario =
+      slices().matching("..org.matheclipse.(*)..").namingSlices("$1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_in_complex_scenario = slices()
+      .matching("..(org.matheclipse).(*)..").namingSlices("$2 of $1").should().beFreeOfCycles();
+
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule no_cycles_in_freely_customized_slices = slices()
+      .assignedFrom(inComplexSliceOneOrTwo()).namingSlices("$1[$2]").should().beFreeOfCycles();
+
+  private static SliceAssignment inComplexSliceOneOrTwo() {
+    return new SliceAssignment() {
+      @Override
+      public String getDescription() {
+        return "complex slice one or two";
+      }
+
+      @Override
+      public SliceIdentifier getIdentifierOf(JavaClass javaClass) {
+        if (javaClass.getPackageName().contains("complexcycles.slice1")) {
+          return SliceIdentifier.of("Complex-Cycle", "One");
+        }
+        if (javaClass.getPackageName().contains("complexcycles.slice2")) {
+          return SliceIdentifier.of("Complex-Cycle", "Two");
+        }
+        return SliceIdentifier.ignore();
+      }
+    };
+  }
 }
