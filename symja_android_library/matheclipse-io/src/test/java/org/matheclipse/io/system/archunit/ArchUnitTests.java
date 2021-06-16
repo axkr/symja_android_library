03fe14c4dcf7f626a14418b0db221732124d6efd
Hannes Wellmann <wellmann.hannes1@gmx.net>
2021-06-16 15:32:36 +0200

    [Tests] improve and speed up ArchUnit tests (#240)
    + use static fields annotated with @ArchTest and @AnalyzeClasses to
    define all rules.
     - to enable this ArchUnitTests simply operatates on all classes in any
     "org" package of this library.
     - this cuts the runtime approximatly to one third because the classes
     are cached between tests
     See https://www.archunit.org/userguide/html/000_Index.html#_junit_support
    
    + disable failing tests instead of commenting them out

--- symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/ArchUnitTests.java
+++ symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/archunit/ArchUnitTests.java
@@ -2,221 +2,103 @@ package org.matheclipse.io.system.archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
-
-import org.matheclipse.core.expression.F;
-
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import org.junit.runner.RunWith;
 import com.tngtech.archunit.base.DescribedPredicate;
 import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchIgnore;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.junit.ArchUnitRunner;
 import com.tngtech.archunit.lang.ArchRule;
 
-import junit.framework.TestCase;
+@RunWith(ArchUnitRunner.class)
+@AnalyzeClasses(packages = "org")
+public class ArchUnitTests {
 
-public class ArchUnitTests extends TestCase {
-  public ArchUnitTests() {
-    super("ArchUnitTests");
-  }
+  @ArchTest
+  public static final ArchRule consoleMethods = methods().that() //
+      .areDeclaredIn(org.matheclipse.io.eval.Console.class).and().doNotHaveName("main") //
+      .should().notBePublic().andShould().notBeProtected();
 
-  public void testConsoleMethods() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse.io.eval");
+  @ArchTest
+  public static final ArchRule MMAConsoleMethods = methods().that() //
+      .areDeclaredIn(org.matheclipse.io.eval.MMAConsole.class).and().doNotHaveName("main") //
+      .should().notBePublic().andShould().notBeProtected();
 
-    ArchRule rule =
-        methods()
-            .that()
-            .areDeclaredIn("org.matheclipse.io.eval.Console")
-            .and()
-            .doNotHaveName("main")
-            .should()
-            .notBePublic()
-            .andShould()
-            .notBeProtected();
-    rule.check(importedClasses);
-  }
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule expession_data_access = classes().that() //
+      .resideInAPackage("org.matheclipse.core.expression.data..") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
-  public void testMMAConsoleMethods() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse.io.eval");
+  @ArchTest
+  public static final ArchRule expession_access =
+      classes().that().resideInAPackage("org.matheclipse.core.expression..")
+          .and(new DescribedPredicate<JavaClass>("") {
+            @Override
+            public boolean apply(JavaClass arg0) {
+              return arg0.getSimpleName().equals("AST") || arg0.getSimpleName().equals("ExprField")
+                  || arg0.getSimpleName().equals("ExprID");
+            }
+          })
+          // TODO change dependency from "org.matheclipse.core.convert"
+          .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
-    ArchRule rule =
-        methods()
-            .that()
-            .areDeclaredIn("org.matheclipse.io.eval.MMAConsole")
-            .and()
-            .doNotHaveName("main")
-            .should()
-            .notBePublic()
-            .andShould()
-            .notBeProtected();
-    rule.check(importedClasses);
-  }
+  @ArchTest
+  public static final ArchRule AST0 = classes().that() //
+      .resideInAPackage("org.matheclipse.core.expression..").and().haveSimpleName("AST0") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
-//  public void test_expession_data_access() {
-//    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-//
-//    ArchRule myRule =
-//        classes()
-//            .that()
-//            .resideInAPackage("org.matheclipse.core.expression.data..")
-//            .should()
-//            .onlyBeAccessed()
-//            .byAnyPackage("org.matheclipse.core.expression..");
-//
-//    myRule.check(importedClasses);
-//  }
+  @ArchTest
+  public static final ArchRule AST1 = classes().that() //
+      .resideInAPackage("org.matheclipse.core.expression..").and().haveSimpleName("AST1") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
-  public void test_expession_access() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
+  @ArchTest
+  public static final ArchRule AST2 = classes().that() //
+      .resideInAPackage("org.matheclipse.core.expression..").and().haveSimpleName("AST2") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.matheclipse.core.expression..")
-            .and(
-                new DescribedPredicate<JavaClass>("") {
-                  @Override
-                  public boolean apply(JavaClass arg0) {
-                    return arg0.getSimpleName().equals("AST")
-                        || arg0.getSimpleName().equals("ExprField")
-                        || arg0.getSimpleName().equals("ExprID");
-                  }
-                })
-            // TODO change dependency from "org.matheclipse.core.convert"
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.matheclipse.core.expression..");
-
-    myRule.check(importedClasses);
-  }
-
-  public void testAST0() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.matheclipse.core.expression..")
-            .and()
-            .haveSimpleName("AST0")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.matheclipse.core.expression..");
-
-    myRule.check(importedClasses);
-  }
-
-  public void testAST1() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.matheclipse.core.expression..")
-            .and()
-            .haveSimpleName("AST1")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.matheclipse.core.expression..");
-
-    myRule.check(importedClasses);
-  }
-
-  public void testAST2() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.matheclipse.core.expression..")
-            .and()
-            .haveSimpleName("AST2")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.matheclipse.core.expression..");
-
-    myRule.check(importedClasses);
-  }
-
-  public void testAST3() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.matheclipse.core.expression..")
-            .and()
-            .haveSimpleName("AST3")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.matheclipse.core.expression..");
-
-    myRule.check(importedClasses);
-  }
+  @ArchTest
+  public static final ArchRule AST3 = classes().that() //
+      .resideInAPackage("org.matheclipse.core.expression..").and().haveSimpleName("AST3") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse.core.expression..");
 
   /**
    * <p>
-   * Split in packages <code>org.matheclipse.core..</code> and <code>org.matheclipse.parser..</code>.
-   * <code>org.matheclipse.parser..</code> should not call Symja <code>IExpr</code> object hierarchy.
+   * Split in packages <code>org.matheclipse.core..</code> and
+   * <code>org.matheclipse.parser..</code>. <code>org.matheclipse.parser..</code> should not call
+   * Symja <code>IExpr</code> object hierarchy.
    * </p>
-   * <b>Note:</b>The <code>ExprParser</code> in package <code>org.matheclipse.core.parser..</code> is allowed to call
-   * Symja <code>IExpr</<code> object hierarchy.
+   * <b>Note:</b>The <code>ExprParser</code> in package <code>org.matheclipse.core.parser..</code>
+   * is allowed to call Symja <code>IExpr</<code> object hierarchy.
    */
-  public void testNoIExprInParser() {
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
+  @ArchTest
+  public static final ArchRule noIExprInParser = classes().that() //
+      .haveSimpleName("IExpr").or().haveSimpleName("IAST") //
+      .should().onlyBeAccessed().byAnyPackage("org.matheclipse..");
 
-    ArchRule rule =
-        classes()
-            .that()
-            .haveSimpleName("IExpr")
-            .or()
-            .haveSimpleName("IAST")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage( //
-                "org.matheclipse..");
-    rule.check(importedClasses);
-  }
+  // LogicNG library
+  @ArchTest
+  public static final ArchRule LogicNG = classes().that() //
+      .resideInAPackage("org.logicng..") //
+      .should().onlyBeAccessed().byAnyPackage("org.logicng..", "..core.builtin..");
 
-  public void testLogicNG() {
-    // LogicNG library
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org");
+  // JGraphT library
+  // TODO reduce package dependencies
+  @ArchTest
+  public static final ArchRule jGraphT = classes().that().resideInAPackage("org.jgrapht..") //
+      .should().onlyBeAccessed().byAnyPackage( //
+          "org.jgrapht..", //
+          "..io.builtin..", //
+          "..core.builtin..", //
+          "..core.expression", //
+          "..core.expression.data", //
+          "..core.reflection.system..");
 
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.logicng..")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage("org.logicng..", "..core.builtin..");
-
-    myRule.check(importedClasses);
-  }
-
-  public void testJGraphT() {
-    // JGraphT library
-    // TODO reduce package dependencies
-    JavaClasses importedClasses = new ClassFileImporter().importPackages("org");
-
-    ArchRule myRule =
-        classes()
-            .that()
-            .resideInAPackage("org.jgrapht..")
-            .should()
-            .onlyBeAccessed()
-            .byAnyPackage( //
-                "org.jgrapht..", //
-                "..io.builtin..", //
-                "..core.builtin..", //
-                "..core.expression", //
-                "..core.expression.data", //
-                "..core.reflection.system..");
-
-    myRule.check(importedClasses);
-  }
-
-  // public void testCycleDependency() {
-  // JavaClasses importedClasses = new ClassFileImporter().importPackages("org.matheclipse");
-  // ArchRule myRule = slices().matching("..matheclipse.(*)..").should().beFreeOfCycles();
-  // myRule.check(importedClasses);
-  // }
-
+  @ArchIgnore
+  @ArchTest
+  public static final ArchRule cycleDependency = slices().matching("..matheclipse.(*)..") //
+      .should().beFreeOfCycles();
 }
