9cef6f3fc9c77346389257b2840d0f9c22fc0c78
Hannes Wellmann <wellmann@iils.de>
2022-01-09 13:33:31 +0100

    [CleanUp] Use Java-11 capabilities in MathScriptEngineFactory

--- symja_android_library/matheclipse-io/src/main/java/org/matheclipse/script/engine/MathScriptEngineFactory.java
+++ symja_android_library/matheclipse-io/src/main/java/org/matheclipse/script/engine/MathScriptEngineFactory.java
@@ -1,6 +1,5 @@
 package org.matheclipse.script.engine;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.script.ScriptEngine;
 import org.matheclipse.core.basic.Config;
@@ -28,10 +27,7 @@ public class MathScriptEngineFactory implements javax.script.ScriptEngineFactory
 
   @Override
   public List<String> getExtensions() {
-    final ArrayList<String> extList = new ArrayList<String>();
-    extList.add("m");
-    extList.add("nb");
-    return extList;
+    return List.of("m", "nb");
   }
 
   @Override
@@ -46,30 +42,17 @@ public class MathScriptEngineFactory implements javax.script.ScriptEngineFactory
 
   @Override
   public String getMethodCallSyntax(String obj, String m, String... args) {
-    StringBuilder callSyntax = new StringBuilder(obj);
-    callSyntax.append(".").append(m).append("(");
-    for (int i = 0; i < args.length; i++) {
-      callSyntax.append(args[i]);
-      if (i < args.length - 2) {
-        callSyntax.append(", ");
-      }
-    }
-    callSyntax.append(")");
-    return callSyntax.toString();
+    return obj + "." + m + "(" + String.join(", ", args) + ")";
   }
 
   @Override
   public List<String> getMimeTypes() {
-    final ArrayList<String> extList = new ArrayList<String>();
-    extList.add("code/matheclipse");
-    return extList;
+    return List.of("code/matheclipse");
   }
 
   @Override
   public List<String> getNames() {
-    final ArrayList<String> extList = new ArrayList<String>();
-    extList.add("matheclipse script");
-    return extList;
+    return List.of("matheclipse script");
   }
 
   @Override
@@ -79,33 +62,25 @@ public class MathScriptEngineFactory implements javax.script.ScriptEngineFactory
 
   @Override
   public Object getParameter(final String key) {
-    if (key.equals(ScriptEngine.ENGINE)) {
-      return getEngineName();
-    } else if (key.equals(ScriptEngine.ENGINE_VERSION)) {
-      return getEngineVersion();
-    } else if (key.equals(ScriptEngine.NAME)) {
-      return getNames();
-    } else if (key.equals(ScriptEngine.LANGUAGE)) {
-      return getLanguageName();
-    } else if (key.equals(ScriptEngine.LANGUAGE_VERSION)) {
-      return getLanguageVersion();
-    } else {
-      return null;
+    switch (key) {
+      case ScriptEngine.ENGINE:
+        return getEngineName();
+      case ScriptEngine.ENGINE_VERSION:
+        return getEngineVersion();
+      case ScriptEngine.NAME:
+        return getNames();
+      case ScriptEngine.LANGUAGE:
+        return getLanguageName();
+      case ScriptEngine.LANGUAGE_VERSION:
+        return getLanguageVersion();
+      default:
+        return null;
     }
   }
 
   @Override
   public String getProgram(String... statements) {
-    final StringBuilder retval = new StringBuilder();
-    final int len = statements.length;
-    for (int i = 0; i < len; i++) {
-      if (i == len - 1) {
-        retval.append(statements[i] + "\n");
-      } else {
-        retval.append(statements[i] + ";\n");
-      }
-    }
-    return retval.toString();
+    return String.join(";\n", statements) + "\n";
   }
 
   @Override
