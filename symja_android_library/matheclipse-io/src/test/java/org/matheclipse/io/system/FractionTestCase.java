618165000fe9419ca282af41ceb1a97445132b09
Hannes Wellmann <wellmann@iils.de>
2022-02-03 13:51:57 +0100

    [Tests] clean up and port FractionTestCase to JUnit5 (#239)

--- symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/FractionTestCase.java
+++ symja_android_library/matheclipse-io/src/test/java/org/matheclipse/io/system/FractionTestCase.java
@@ -1,95 +1,69 @@
 package org.matheclipse.io.system;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.matheclipse.core.expression.AbstractFractionSym.valueOf;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.matheclipse.core.eval.exception.ArgumentTypeException;
-import org.matheclipse.core.expression.AbstractFractionSym;
 import org.matheclipse.core.interfaces.IFraction;
 
 /**
  * Tests for the Java port of the <a href="http://www.apmaths.uwo.ca/~arich/">Rubi - rule-based
  * integrator</a>.
  */
-public class FractionTestCase extends AbstractTestCase {
-  public FractionTestCase(String name) {
-    super(name);
+class FractionTestCase {
+
+  @Test
+  void testCTor() {
+    assertEquals("1", valueOf(Integer.MIN_VALUE, Integer.MIN_VALUE));
+
+    assertEquals("1", valueOf(Integer.MAX_VALUE, Integer.MAX_VALUE));
+
+    assertEquals("-2147483647/2147483648", valueOf(Integer.MAX_VALUE, Integer.MIN_VALUE));
+
+    assertEquals("-2147483648/2147483647", valueOf(Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+    assertEquals("1", valueOf(Long.MIN_VALUE, Long.MIN_VALUE));
+
+    assertEquals("1", valueOf(Long.MAX_VALUE, Long.MAX_VALUE));
+
+    assertEquals("-9223372036854775807/9223372036854775808",
+        valueOf(Long.MAX_VALUE, Long.MIN_VALUE));
+
+    assertEquals("-9223372036854775808/9223372036854775807",
+        valueOf(Long.MIN_VALUE, Long.MAX_VALUE));
+
+    assertEquals("1", valueOf(Integer.MIN_VALUE - 1L, Integer.MIN_VALUE - 1L));
+
+    assertEquals("1", valueOf(Integer.MAX_VALUE + 1L, Integer.MAX_VALUE + 1L));
+
+    assertEquals("-2147483648/2147483649", valueOf(Integer.MAX_VALUE + 1L, Integer.MIN_VALUE - 1L));
+
+    assertEquals("-2147483649/2147483648", valueOf(Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L));
   }
 
-  public void testCTor() {
-    IFraction a = AbstractFractionSym.valueOf(Integer.MIN_VALUE, Integer.MIN_VALUE);
-    assertEquals("1", a.toString());
+  @Test
+  void testCTor0() {
+    assertEquals("0", valueOf(0, Integer.MIN_VALUE));
 
-    a = AbstractFractionSym.valueOf(Integer.MAX_VALUE, Integer.MAX_VALUE);
-    assertEquals("1", a.toString());
+    assertThrows(ArgumentTypeException.class, () -> valueOf(Integer.MAX_VALUE, 0));
 
-    a = AbstractFractionSym.valueOf(Integer.MAX_VALUE, Integer.MIN_VALUE);
-    assertEquals("-2147483647/2147483648", a.toString());
+    assertThrows(ArgumentTypeException.class, () -> valueOf(Integer.MIN_VALUE, 0));
 
-    a = AbstractFractionSym.valueOf(Integer.MIN_VALUE, Integer.MAX_VALUE);
-    assertEquals("-2147483648/2147483647", a.toString());
-
-    a = AbstractFractionSym.valueOf(Long.MIN_VALUE, Long.MIN_VALUE);
-    assertEquals("1", a.toString());
-
-    a = AbstractFractionSym.valueOf(Long.MAX_VALUE, Long.MAX_VALUE);
-    assertEquals("1", a.toString());
-
-    a = AbstractFractionSym.valueOf(Long.MAX_VALUE, Long.MIN_VALUE);
-    assertEquals("-9223372036854775807/9223372036854775808", a.toString());
-
-    a = AbstractFractionSym.valueOf(Long.MIN_VALUE, Long.MAX_VALUE);
-    assertEquals("-9223372036854775808/9223372036854775807", a.toString());
-
-    a = AbstractFractionSym.valueOf(Integer.MIN_VALUE - 1L, Integer.MIN_VALUE - 1L);
-    assertEquals("1", a.toString());
-
-    a = AbstractFractionSym.valueOf(Integer.MAX_VALUE + 1L, Integer.MAX_VALUE + 1L);
-    assertEquals("1", a.toString());
-
-    a = AbstractFractionSym.valueOf(Integer.MAX_VALUE + 1L, Integer.MIN_VALUE - 1L);
-    assertEquals("-2147483648/2147483649", a.toString());
-
-    a = AbstractFractionSym.valueOf(Integer.MIN_VALUE - 1L, Integer.MAX_VALUE + 1L);
-    assertEquals("-2147483649/2147483648", a.toString());
-  }
-
-  public void testCTor0() {
-    IFraction a = AbstractFractionSym.valueOf(0, Integer.MIN_VALUE);
-    assertEquals("0", a.toString());
-
-    try {
-      a = AbstractFractionSym.valueOf(Integer.MAX_VALUE, 0);
-      fail();
-    } catch (ArgumentTypeException ate) {
-
-    }
-
-    try {
-      a = AbstractFractionSym.valueOf(Integer.MIN_VALUE, 0);
-      fail();
-    } catch (ArgumentTypeException ate) {
-
-    }
-    a = AbstractFractionSym.valueOf(0, Integer.MAX_VALUE);
-    assertEquals("0", a.toString());
+    assertEquals("0", valueOf(0, Integer.MAX_VALUE));
 
     // Long
 
-    a = AbstractFractionSym.valueOf(0, Long.MIN_VALUE);
-    assertEquals("0", a.toString());
+    assertEquals("0", valueOf(0, Long.MIN_VALUE));
 
-    try {
-      a = AbstractFractionSym.valueOf(Long.MAX_VALUE, 0);
-      fail();
-    } catch (ArgumentTypeException ate) {
+    assertThrows(ArgumentTypeException.class, () -> valueOf(Long.MAX_VALUE, 0));
 
-    }
+    assertThrows(ArgumentTypeException.class, () -> valueOf(Long.MIN_VALUE, 0));
 
-    try {
-      a = AbstractFractionSym.valueOf(Long.MIN_VALUE, 0);
-      fail();
-    } catch (ArgumentTypeException ate) {
+    assertEquals("0", valueOf(0, Long.MAX_VALUE));
+  }
 
-    }
-    a = AbstractFractionSym.valueOf(0, Long.MAX_VALUE);
-    assertEquals("0", a.toString());
+  private static void assertEquals(String expected, IFraction actual) {
+    Assertions.assertEquals(expected, actual.toString());
   }
 }
