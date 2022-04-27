b2f45c74e295a1db490fb9b9a7bba09fd884c811
axexlck <axelclk@gmail.com>
2022-04-27 17:50:05 +0200

    symja-shell.bat use JDK 11

--- symja_android_library/symja-jshell.bat
+++ symja_android_library/symja-jshell.bat
@@ -1,6 +1,6 @@
 REM change directory to symja installation folder
 REM cd /temp/symja
 REM run start-symja.bat
-SET "JAVA_HOME=C:\Program Files\Java\jdk-10.0.1"
+SET "JAVA_HOME=C:\Program Files\Java\jdk-11.0.12"
 
 "%JAVA_HOME%\bin\jshell" --class-path "/temp/symja/lib/*" --startup start-symja.jsh
