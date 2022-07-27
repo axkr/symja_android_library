ac88b6b0d76e7ba8884ff92f766fbbdcf71e43e9
axexlck <axelclk@gmail.com>
2022-07-27 19:02:32 +0200

    define Comparators#LEXICAL_COMPARATOR

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/Comparators.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/generic/Comparators.java
@@ -3,6 +3,7 @@ package org.matheclipse.core.generic;
 import java.util.Comparator;
 import java.util.function.BiPredicate;
 import org.matheclipse.core.builtin.ListFunctions;
+import org.matheclipse.core.interfaces.IAST;
 import org.matheclipse.core.interfaces.IExpr;
 
 /** Provide <code>Comparator<IExpr></code> classes. */
@@ -24,6 +25,12 @@ public final class Comparators {
    */
   public static final ExprReverseComparator REVERSE_CANONICAL_COMPARATOR =
       new ExprReverseComparator();
+
+  /**
+   * Compares the elements of two {@link IAST}s (on the first level only) until the minimum size of
+   * the {@link IAST}s, before considering their sequence lengths.
+   */
+  public static final ExprLexicalComparator LEXICAL_COMPARATOR = new ExprLexicalComparator();
 
   /**
    * Compares an expression with another expression for order. Returns a negative integer, zero, or
@@ -55,6 +62,46 @@ public final class Comparators {
       return o2.compareTo(o1);
     }
   }
+
+  static final class ExprLexicalComparator implements Comparator<IExpr> {
+
+    /**
+     * Compares an expression with another expression for order. Returns a negative integer, zero,
+     * or a positive integer if this expression is canonical greater than, equal to, or less than
+     * the specified expression.
+     */
+    @Override
+    public int compare(final IExpr o1, final IExpr o2) {
+      if (o1.isAST() && o2.isAST()) {
+        IAST lhsAST = (IAST) o1;
+        IAST rhsAST = (IAST) o2;
+
+        // compare the headers
+        int cp = lhsAST.head().compareTo(rhsAST.head());
+        if (cp != 0) {
+          return cp;
+        }
+
+        final int minimumSize = (lhsAST.size() > rhsAST.size()) ? rhsAST.size() : lhsAST.size();
+        for (int i = 1; i < minimumSize; i++) {
+          cp = lhsAST.get(i).compareTo(rhsAST.get(i));
+          if (cp != 0) {
+            return cp;
+          }
+        }
+
+        if (lhsAST.size() > rhsAST.size()) {
+          return 1;
+        }
+        if (lhsAST.size() < rhsAST.size()) {
+          return -1;
+        }
+        return 0;
+      }
+      return o2.compareTo(o1);
+    }
+  }
+
 
   /**
    * Comparator for using an internal {@link BiPredicate} from
