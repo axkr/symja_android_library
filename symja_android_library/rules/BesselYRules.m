f59a0c620892bedc7dfc55a30619b25a34360abe
axexlck <axelclk@gmail.com>
2023-03-19 13:40:41 +0100

    Improved Bessel rules

--- symja_android_library/rules/BesselYRules.m
+++ symja_android_library/rules/BesselYRules.m
@@ -8,5 +8,13 @@
  BesselY(3/2,z_):=(Sqrt(2/Pi)*(-(Cos(z)/z) - Sin(z)))/Sqrt(z),
  
  BesselY(-5/2,z_):=(Sqrt(2/Pi)*(-((3*Cos(z))/z) - Sin(z) + (3*Sin(z))/z^2))/Sqrt(z),
- BesselY(5/2,z_):=(Sqrt(2/Pi)*(Cos(z) - (3*Cos(z))/z^2 - (3*Sin(z))/z))/Sqrt(z)
+ BesselY(5/2,z_):=(Sqrt(2/Pi)*(Cos(z) - (3*Cos(z))/z^2 - (3*Sin(z))/z))/Sqrt(z),
+ 
+  
+ BesselY(x_/;(x>0 && IntegerQ(2*x)),z_/;(z!=0&&FreeQ(z,DirectedInfinity))):=
+   Module({u,f,k= x-1/2},f=Cos(u)/u;While(k>0, k=k-1;f = (D(f, u)/u)); (-Sqrt(2/Pi*z) * ((-u)^(x-1/2)*f))/.u->z),
+   
+ BesselY(x_/;(x<0 && IntegerQ(2*x)),z_/;(z!=0&&FreeQ(z,DirectedInfinity))):=
+   Module({u,f,k=-x-1/2},f=Sin(u)/u;While(k>0, k=k-1;f = (D(f, u)/u)); (Sqrt(2/Pi*z) * ((-u)^(-x-1/2)*f))/.u->z)
+ 
 }
