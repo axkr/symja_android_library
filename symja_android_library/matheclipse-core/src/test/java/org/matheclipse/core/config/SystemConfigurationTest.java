2016d58cf54bcb87f6694f0dcb2fba7a035a7daf
duytran <duytranle.it@gmail.com>
2023-05-12 14:39:53 +0700

    testDeterminePrecision

--- symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/config/SystemConfigurationTest.java
+++ symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/config/SystemConfigurationTest.java
@@ -1,56 +1,60 @@
 package org.matheclipse.core.config;
 
+import junit.framework.TestCase;
 import org.apfloat.Apfloat;
 import org.apfloat.ApfloatMath;
 import org.apfloat.OverflowException;
 import org.matheclipse.core.eval.ExprEvaluator;
 import org.matheclipse.core.expression.ApfloatNum;
 import org.matheclipse.core.expression.F;
+import org.matheclipse.core.interfaces.IASTMutable;
 import org.matheclipse.core.interfaces.IExpr;
 import org.matheclipse.core.interfaces.IInteger;
-import junit.framework.TestCase;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public class SystemConfigurationTest extends TestCase {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        F.initSymbols();
-    }
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    F.initSymbols();
+  }
 
-    public void testApfloatStorage() {
-      // ApfloatContext.getContext().setFilenameGenerator(new FilenameGenerator("default", "0",
-      // "default") {
-      // @Override
-      // public synchronized String generateFilename() {
-      // throw new OverflowException("Apfloat disk file storage is disabled");
-      // }
-      // });
-
-        try {
-            Apfloat apfloat = new Apfloat("9".repeat(1_000_000) + "." + "9".repeat(1_000_000));
-            apfloat = ApfloatMath.pow(apfloat, new Apfloat("91212312" + ".1231236"));
-            ApfloatNum apfloatNum = ApfloatNum.valueOf(apfloat);
-            IInteger integerPart = apfloatNum.integerPart();
-            System.out.println(integerPart.bitLength());
-            fail("Should be fail");
-          } catch (OverflowException e) {
-            e.printStackTrace();
-        }
+  public void testApfloatStorage() {
+    try {
+      Apfloat apfloat = new Apfloat("9".repeat(1_000_000) + "." + "9".repeat(1_000_000));
+      apfloat = ApfloatMath.pow(apfloat, new Apfloat("91212312" + ".1231236"));
+      ApfloatNum apfloatNum = ApfloatNum.valueOf(apfloat);
+      IInteger integerPart = apfloatNum.integerPart();
+      System.out.println(integerPart.bitLength());
+      fail("Should be fail");
+    } catch (OverflowException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void testOverflowError() {
-        String expr = "N(1.7*10^1,100)/N(2.5*10^1,100)*N(0,100)";
-        ExprEvaluator evaluator = new ExprEvaluator();
-        IExpr result = evaluator.eval(expr);
-        assertEquals(result.toString(), "0");
-    }
+  public void testOverflowError() {
+    String expr = "N(1.7*10^1,100)/N(2.5*10^1,100)*N(0,100)";
+    ExprEvaluator evaluator = new ExprEvaluator();
+    IExpr result = evaluator.eval(expr);
+    assertEquals(result.toString(), "0");
+  }
 
-    public void testOverflowError02() {
-        String expr = "ArcSin(N(1.7*10^1,100)/N(2.5*10^1,100)*N(0,100))";
-        ExprEvaluator evaluator = new ExprEvaluator();
-        IExpr result = evaluator.eval(expr);
-        assertEquals(result.toString(), "0");
-    }
+  public void testOverflowError02() {
+    String expr = "ArcSin(N(1.7*10^1,100)/N(2.5*10^1,100)*N(0,100))";
+    ExprEvaluator evaluator = new ExprEvaluator();
+    IExpr result = evaluator.eval(expr);
+    assertEquals(result.toString(), "0");
+  }
+
+  public void testDeterminePrecision() {
+    ApfloatNum zero = ApfloatNum.valueOf(new Apfloat(new BigDecimal(BigInteger.ZERO), 30));
+    ApfloatNum num = ApfloatNum.valueOf("1.7", 30);
+
+    IASTMutable times = F.Times(num, zero);
+    assertEquals(times.determinePrecision(), 30);
+  }
 
 }
