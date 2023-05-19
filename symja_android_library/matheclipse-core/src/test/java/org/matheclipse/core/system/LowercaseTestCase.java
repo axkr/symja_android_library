bc282c21612b907b3562b68f9e2cae84820fc2d3
duytran <duytranle.it@gmail.com>
2023-05-20 00:06:41 +0700

    Update

--- symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
+++ symja_android_library/matheclipse-core/src/test/java/org/matheclipse/core/system/LowercaseTestCase.java
@@ -13938,6 +13938,7 @@ public class LowercaseTestCase extends ExprEvaluatorTestCase {
         "Log(3)");
     check("MangoldtLambda({1,2,3,4,5,6,7,8,9})", //
         "{0,Log(2),Log(3),Log(2),Log(5),0,Log(7),Log(2),Log(3)}");
+
     check("{MangoldtLambda(Prime(10^5)^10), MangoldtLambda(2*Prime(10^5)^10)}", //
         "{Log(1299709),0}");
   }
