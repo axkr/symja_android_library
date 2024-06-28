927842cdfff7f3b703f22cc05503579f598c259b
axexlck <axelclk@gmail.com>
2024-06-28 16:38:17 +0200

    jsxgraph 1.9.2
    - https://github.com/jsxgraph/jsxgraph/releases/tag/v1.9.2

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/form/output/JSBuilder.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/form/output/JSBuilder.java
@@ -256,12 +256,12 @@ public class JSBuilder {
 
   /** CSS libraries */
   private static final String[] CSS_CDN_JSXGRAPH =
-      {"https://cdn.jsdelivr.net/npm/jsxgraph@1.9.1/distrib/jsxgraph.css"};
+      {"https://cdn.jsdelivr.net/npm/jsxgraph@1.9.2/distrib/jsxgraph.css"};
 
   /** CDN JavaScript libraries */
   private static final String[] JS_CDN_JSXGRAPH =
       {"https://cdn.jsdelivr.net/gh/paulmasson/math@1.4.10/build/math.js",
-          "https://cdn.jsdelivr.net/npm/jsxgraph@1.9.1/distrib/jsxgraphcore.js",
+          "https://cdn.jsdelivr.net/npm/jsxgraph@1.9.2/distrib/jsxgraphcore.js",
           "https://cdn.jsdelivr.net/npm/json2d_jsxgraph@1.0.1/drawGraphics2d.js"};
 
   /** CSS libraries */
