592ef50e1df6616efa44b58615b44a080b96f42e
axexlck <axelclk@gmail.com>
2025-10-07 22:25:41 +0200

    Use JUnit 6 jupiter API

--- symja_android_library/matheclipse-api/src/test/java/org/matheclipse/api/TestFuzzyInput.java
+++ symja_android_library/matheclipse-api/src/test/java/org/matheclipse/api/TestFuzzyInput.java
@@ -1,7 +1,7 @@
 package org.matheclipse.api;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 import org.matheclipse.core.eval.EvalEngine;
 import org.matheclipse.core.interfaces.IExpr;
 
--- symja_android_library/matheclipse-api/src/test/java/org/matheclipse/api/TestPods.java
+++ symja_android_library/matheclipse-api/src/test/java/org/matheclipse/api/TestPods.java
@@ -1,6 +1,6 @@
 package org.matheclipse.api;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.regex.Matcher;
