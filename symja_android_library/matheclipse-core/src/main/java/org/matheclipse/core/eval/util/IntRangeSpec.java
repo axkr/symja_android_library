2079506a7435d8eb9044e16946a911e881be8a75
axexlck <axelclk@gmail.com>
2026-07-04 17:19:32 +0200

    Enhance `IntRangeSpec` to support variable step sizes in range creation

--- symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/util/IntRangeSpec.java
+++ symja_android_library/matheclipse-core/src/main/java/org/matheclipse/core/eval/util/IntRangeSpec.java
@@ -7,6 +7,7 @@ import org.matheclipse.core.interfaces.IExpr;
 public class IntRangeSpec {
   final int min;
   final int max;
+  final int step;
 
   public int minimum() {
     return min;
@@ -16,43 +17,26 @@ public class IntRangeSpec {
     return max;
   }
 
-  /**
-   * Create the range <code>[min, max]</code>
-   *
-   * @return the range <code>[min, max]</code>
-   */
+  public int step() {
+    return step;
+  }
+
   public IntRangeSpec(int min, int max) {
     this.min = min;
     this.max = max;
+    this.step = 1;
   }
 
-  /**
-   * Create the default non-negative range <code>[0, Integer.MAX_VALUE]</code>
-   *
-   * @return the range <code>[0, Integer.MAX_VALUE]</code>
-   */
+  public IntRangeSpec(int min, int max, int step) {
+    this.min = min;
+    this.max = max;
+    this.step = step;
+  }
+
   public static IntRangeSpec createNonNegative() {
-    return new IntRangeSpec(0, Integer.MAX_VALUE);
+    return new IntRangeSpec(0, Integer.MAX_VALUE, 1);
   }
 
-  /**
-   * Determine a non-negative range in the interval <code>[0, Integer.MAX_VALUE]</code> from <code>
-   * ast.get(position)</code>. If some parts of the range are negative or undefined return <code>
-   * null</code>.
-   *
-   * <ul>
-   * <li>&quot;All&quot; - gives <code>[0, Integer.MAX_VALUE]</code>
-   * <li>&quot;int value&quot; - gives exactly <code>[0, &quot;int value&quot;]</code>
-   * <li>&quot;{int value}&quot; - gives <code>[&quot;int value&quot;, &quot;int value&quot;]
-   *       </code>
-   * <li>&quot;{min, max}&quot; - gives <code>[&quot;min&quot;, &quot;max&quot;]</code>
-   * </ul>
-   *
-   * @param ast
-   * @param position
-   * @return <code>null</code> if no <code>int</code> range in the interval <code>
-   *     [0, Integer.MAX_VALUE]</code> could be determined
-   */
   public static IntRangeSpec createNonNegative(IAST ast, int position) {
     IntRangeSpec range = null;
     if (ast.size() <= position) {
@@ -63,57 +47,64 @@ public class IntRangeSpec {
     return range;
   }
 
-  /**
-   * Determine a non-negative range in the interval <code>[0, Integer.MAX_VALUE]</code>. If some
-   * parts of the range are negative or undefined return <code>null</code>.
-   *
-   * <ul>
-   * <li>&quot;All&quot; - gives <code>[0, Integer.MAX_VALUE]</code>
-   * <li>&quot;int value&quot; - gives exactly <code>[0, &quot;int value&quot;]</code>
-   * <li>&quot;{int value}&quot; - gives <code>[&quot;int value&quot;, &quot;int value&quot;]
-   *       </code>
-   * <li>&quot;{min, max}&quot; - gives <code>[&quot;min&quot;, &quot;max&quot;]</code>
-   * </ul>
-   *
-   * @param specification
-   * @return <code>null</code> if no <code>int</code> range in the interval <code>
-   *     [0, Integer.MAX_VALUE]</code> could be determined
-   */
   public static IntRangeSpec createNonNegative(IExpr specification) {
     int min = 0;
     int max = Integer.MAX_VALUE;
+    int step = 1;
+
     if (specification.equals(S.All) || specification.isInfinity()) {
       // all from 0 to Integer.MAX_VALUE
     } else if (specification.isInteger()) {
-      // k - at most k elements
       max = specification.toIntDefault(-1);
-      if (max < 0) {
+      if (max < 0)
         return null;
-      }
     } else if (specification.isList1()) {
-      // {k} - exactly k
       min = specification.first().toIntDefault(-1);
-      if (min < 0) {
+      if (min < 0)
         return null;
-      }
       max = min;
-    } else if (specification.isList2()) {
-      // {min, max}
-      min = specification.first().toIntDefault(-1);
-      if (min < 0) {
+    } else if (specification.isList2() || specification.isList3()) {
+      IAST list = (IAST) specification;
+      min = list.first().toIntDefault(-1);
+      if (min < 0)
         return null;
+
+      int parsedStep = 1;
+      if (specification.isList3()) {
+        parsedStep = list.arg3().toIntDefault(0);
+        if (parsedStep == 0)
+          return null; // Step cannot be mathematically 0
       }
-      max = specification.second().toIntDefault(-1);
-      if (max < 0) {
-        return null;
+      step = parsedStep;
+
+      IExpr arg2 = list.second();
+      if (arg2.isInfinity() || arg2.equals(S.All)) {
+        max = step > 0 ? Integer.MAX_VALUE : 0;
+      } else {
+        max = arg2.toIntDefault(-1);
+        if (max < 0)
+          return null;
       }
     } else {
       return null;
     }
-    return new IntRangeSpec(min, max);
+    return new IntRangeSpec(min, max, step);
   }
 
   public boolean isIncluded(int value) {
-    return (min <= value && value <= max);
+    if (step > 0) {
+      if (min <= value && value <= max) {
+        if (step == 1)
+          return true;
+        return (value - min) % step == 0;
+      }
+    } else if (step < 0) {
+      if (min >= value && value >= max) {
+        if (step == -1)
+          return true;
+        return (value - min) % step == 0;
+      }
+    }
+    return false;
   }
 }
